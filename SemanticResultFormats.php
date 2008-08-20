b76326233b9500258e31bd88bbffbbfba62082d9
Denny Vrandecic <vrandezo@users.mediawiki.org>
2008-08-20 21:50:35 +0000

    Initial commit of the Google Charts visualizer and the graphs extension

---
+++ SemanticResultFormats.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Initializing file for the diverse query result formats supported in this
+ * extension. In order to use them, add the following lines to your
+ * LocalSettings.php:
+ * 
+ *  ....
+ * 
+ */
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+$smwgResultFormats['graph'] = 'SMWGraphResultPrinter';
+$smwgResultFormats['googlebar'] = 'SMWGoogleBarResultPrinter';
+$smwgResultFormats['googlepie'] = 'SMWGooglePieResultPrinter';
+
+include_once( 'GoogleCharts/GoogleCharts.php' );
+include_once( 'GraphViz/Graph.php' );
