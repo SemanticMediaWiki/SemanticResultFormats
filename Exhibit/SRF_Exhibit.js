18c2762d3814ed944bcb50639bf8dc2d1cbfe3fd
Markus Krötzsch <mkroetzsch@users.mediawiki.org>
2009-03-25 14:43:49 +0000

    Updated Exhibit (by Fabian Howahl); include JavaScripts/styles/images now; fixes for IE8; various fixes and improvements. Note: Exhibit still contacts external servers for GoogleMap view (obviously) and for Timeline view (not so obvious, requires a fix).

--- Exhibit/SRF_Exhibit.js
+++ Exhibit/SRF_Exhibit.js
@@ -5,39 +5,39 @@ function createExhibit() {//overload Exhibit's autoCreate-functionality
    * Data: We're using the HTML table importer to get the data for the exhibit.
   */
  
-  window.database = Exhibit.Database.create();
-  window.exhibit = Exhibit.create(window.database);
-    for (var id in sources) {
-      var source = sources[id];
-      var dataTable = document.getElementById(source.id);
-      if (source.hideTable !== "false") { dataTable.setAttribute("style", "display:none"); };
-	dataTable.setAttribute("ex:type", source.type);
-	dataTable.setAttribute("ex:label", source.label);
-	dataTable.setAttribute("ex:pluralLabel", source.pluralLabel);
-	var th, ths = dataTable.getElementsByTagName("th");
-	var columns = source.columns;
-	if (columns[0] !== "") {
-	  for(var c = 0; c < ths.length; c++) {
-	    var split = columns[c].indexOf(':');
-	    if (split > 0) {
-	      var name = columns[c].substring(0, split);
-	      var valueType = columns[c].substring((split + 1), columns[c].length);
-	      ths[c].setAttribute('ex:valueType', valueType);
-	    } else { var name = columns[c]; }
-	    ths[c].setAttribute('ex:name', name);
-	  }	
-	} else {
-	  ths[0].setAttribute('ex:name', 'label');
-	  for (var c = 1; c < ths.length; c++) {
-	    //Safari uses innerText instead of textContent, so:
-	    var thetext = ths[c].textContent || ths[c].innerText;
-	    var label = thetext.toLowerCase();
-	    label = label.replace(/\s/g,'');
-	    ths[c].setAttribute('ex:name', label);
-	  }
+	window.database = Exhibit.Database.create();
+	window.exhibit = Exhibit.create(window.database);
+	for (var id in sources) {
+		var source = sources[id];
+		var dataTable = document.getElementById(source.id);
+		//if (source.hideTable == 1) { dataTable.setAttribute("style", "display:none"); };
+		dataTable.setAttribute("ex:type", source.type);
+		dataTable.setAttribute("ex:label", source.label);
+		dataTable.setAttribute("ex:pluralLabel", source.pluralLabel);
+		var th, ths = dataTable.getElementsByTagName("th");
+		var columns = source.columns;
+		if (columns[0] !== "") {
+			for(var c = 0; c < ths.length; c++) {
+				var split = columns[c].indexOf(':');
+				if (split > 0) {
+					var name = columns[c].substring(0, split);
+					var valueType = columns[c].substring((split + 1), columns[c].length);
+					ths[c].setAttribute('ex:valueType', valueType);
+				} else { var name = columns[c]; }
+				ths[c].setAttribute('ex:name', name);
+			}				
+		} else {
+			ths[0].setAttribute('ex:name', 'label');
+			for (var c = 1; c < ths.length; c++) {
+				//Safari uses innerText instead of textContent, so:
+				var thetext = ths[c].textContent || ths[c].innerText;
+				var label = thetext.toLowerCase();
+				label = label.replace(/\s/g,'');
+				ths[c].setAttribute('ex:name', label);
+			}
+		}
+		Exhibit.HtmlTableImporter.loadTable(dataTable, window.database); 
 	}
-	Exhibit.HtmlTableImporter.loadTable(dataTable, window.database); 
-    }
 
     window.database.loadDataLinks(); //load JSON files in addition
 	
@@ -50,50 +50,58 @@ function createExhibit() {//overload Exhibit's autoCreate-functionality
      */
     if (facets) {//facets
       for (var index in facets) {
-	var facet = facets[index];
-	var position = facet.position;
-	var innerHTML = "<div " + facet.innerHTML + " style='padding: 2px; float: left; width: 15em' ></div>";
-	if (position == "top") {
-	  var facetDiv = document.getElementById('top-facets')
-	    facetDiv.innerHTML = facetDiv.innerHTML + innerHTML;
-	}
-	if (position == "bottom") {
-	  var facetDiv = document.getElementById('bottom-facets');
-	  facetDiv.innerHTML = facetDiv.innerHTML + innerHTML;
-	}
-	if (position == "left") {
-	  var facetTd = document.getElementById('left-facets');
-	  facetTd.innerHTML = facetTd.innerHTML + innerHTML;
-	  facetTd.setAttribute('width', '20%');
-	}
-	if (position == "right") {
-	  var facetTd = document.getElementById('right-facets');
-	  facetTd.innerHTML = facetTd.innerHTML + innerHTML;
-	  facetTd.setAttribute('width', '20%');
-	}
-      }
+		  var facet = facets[index];
+		  var position = facet.position;
+		  var innerHTML = "<div " + facet.innerHTML + " style='padding: 2px; float: left; width: 15em' ></div>";
+		  if (position == "top") {
+			  var facetDiv = document.getElementById('top-facets')
+			  facetDiv.innerHTML = facetDiv.innerHTML + innerHTML;
+		  }
+		  if (position == "bottom") {
+			  var facetDiv = document.getElementById('bottom-facets');
+			  facetDiv.innerHTML = facetDiv.innerHTML + innerHTML;
+		  }
+		  if (position == "left") {
+			  var facetTd = document.getElementById('left-facets');
+			  facetTd.innerHTML = facetTd.innerHTML + innerHTML;
+			  facetTd.setAttribute('width', '20%');
+		  }
+		  if (position == "right") {
+			  var facetTd = document.getElementById('right-facets');
+			  facetTd.innerHTML = facetTd.innerHTML + innerHTML;
+			  facetTd.setAttribute('width', '20%');
+		  }
+		}
     }
 	
     if (views && (views[0] !== "")) {//views
-      var viewHTML = '<div id="exhibit-view-panel" ex:role="viewPanel"><div ex:role="lens">'+lens+'</div>';
-      for (var i = 0; i < views.length; i++) {
-	viewHTML = viewHTML + '<div ' + views[i] + ' ></div>';
-      }
-      viewHTML = viewHTML + '</div>';
-      document.getElementById("view").innerHTML = viewHTML;
+		var viewHTML = '<div id="exhibit-view-panel" ex:role="viewPanel"><div ex:role="lens">'+lens+'</div>';
+		for (var i = 0; i < views.length; i++) {
+			viewHTML = viewHTML + '<div ' + views[i] + ' ></div>';
+		}
+		viewHTML = viewHTML + '</div>';
+		document.getElementById("view").innerHTML = viewHTML;
     } else {
-      document.getElementById("view").innerHTML = '<div ex:role="view"></div>';
+		document.getElementById("view").innerHTML = '<div ex:role="view"></div>';
     }
     
-    for(var i = 0; i < counter; i++){ //lenses
-      var test = document.getElementById("lenscontent"+i);
-      test.setAttribute('ex:content','.' + test.innerHTML);
-      test.innerHTML = '';
+    for(var i = 0; i < lenscounter; i++){ //lenses
+		var test = document.getElementById("lenscontent"+i);
+		test.setAttribute('ex:content','.' + test.innerHTML);
+		test.setAttribute('class',"inlines");
+		test.innerHTML = '';
     }
-	  
+    for(var i = 0; i < linkcounter; i++){ //lenses
+		var test = document.getElementById("linkcontent"+i);
+		var newlink = document.createElement('a');
+		newlink.setAttribute('ex:href-subcontent',wgServer + wgScript + '?title={{.' + test.innerHTML + '}}');
+		newlink.setAttribute('ex:content','.' + test.innerHTML);
+		test.innerHTML = '';
+		test.appendChild(newlink);
+    }
+	
     window.exhibit.configureFromDOM();
 }
-
 
 addOnloadHook(createExhibit);
 
