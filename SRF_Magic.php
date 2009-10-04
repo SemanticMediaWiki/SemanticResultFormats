faf006c6e31600df09a9ac6b069915b945cb21f2
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-10-04 22:45:32 +0000

    Localisation updates for extension magic words from translatewiki.net

--- SRF_Magic.php
+++ SRF_Magic.php
@@ -12,6 +12,11 @@ $magicWords['ar'] = array(
 	'calendarenddate'     => array( '0', 'تاريخ_نهاية_التقويم', 'calendarenddate' ),
 );
 
+$magicWords['mr'] = array(
+	'calendarstartdate'   => array( '0', 'दिनदर्शिकारंभदिन', 'calendarstartdate' ),
+	'calendarenddate'     => array( '0', 'दिनदर्शिकासांगतादिन', 'calendarenddate' ),
+);
+
 $magicWords['nl'] = array(
 	'calendarstartdate'   => array( '0', 'startdatumkalender', 'calendarstartdate' ),
 	'calendarenddate'     => array( '0', 'einddatumkalender', 'calendarenddate' ),
