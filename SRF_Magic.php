f1c1e2c3e976fad1ec6577f069707659331d42a8
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-11-07 00:44:29 +0000

    Localisation updates for extension messages from translatewiki.net * full export and rebuild of all supported messages, magic and special pages aliases files

--- SRF_Magic.php
+++ SRF_Magic.php
@@ -17,6 +17,11 @@ $magicWords['mr'] = array(
 	'calendarenddate'     => array( '0', 'दिनदर्शिकासांगतादिन', 'calendarenddate' ),
 );
 
+$magicWords['nds-nl'] = array(
+	'calendarstartdate'   => array( '0', 'startdaotumkelender', 'startdatumkalender', 'calendarstartdate' ),
+	'calendarenddate'     => array( '0', 'einddaotumkelender', 'einddatumkalender', 'calendarenddate' ),
+);
+
 $magicWords['nl'] = array(
 	'calendarstartdate'   => array( '0', 'startdatumkalender', 'calendarstartdate' ),
 	'calendarenddate'     => array( '0', 'einddatumkalender', 'calendarenddate' ),
