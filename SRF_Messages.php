805a72649dccff3b0f3fb67577d17c78ed29cee2
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-02-08 21:26:34 +0000

    typo fix

--- SRF_Messages.php
+++ SRF_Messages.php
@@ -77,9 +77,9 @@ $messages['en'] = array(
 	'srf_printername_tagcloud' => 'Tag cloud',
 	'srf_paramdesc_increase' => 'How to increase the size of tags',
 	'srf_paramdesc_tagorder' => 'The order of the tags',
-	'srf_paramdesc_mincount' => 'The minimun amount of times a value needs to occur to be listed',
+	'srf_paramdesc_mincount' => 'The minimum amount of times a value needs to occur to be listed',
 	'srf_paramdesc_increasefactor' => 'How many percent to increase the size of a tag per step',
-	'srf_paramdesc_maxtags' => 'The maximun amount of tags in the cloud',
+	'srf_paramdesc_maxtags' => 'The maximum amount of tags in the cloud',
 	'srf_paramdesc_minsize' => 'The size of the smallest tags in percent (default: 77)',
 );
 
