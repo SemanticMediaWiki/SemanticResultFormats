052917d7891614e4039117652dbd8f99c6925c24
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-02-08 17:20:20 +0000

    Follow up to  r81762

--- TagCloud/SRF_TagCloud.php
+++ TagCloud/SRF_TagCloud.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * Print query results as a tag cloud.
+ * Result printer that prints query results as a tag cloud.
  * 
  * @since 1.5.3
  * 
@@ -9,12 +10,6 @@
  * 
  * @licence GNU GPL v3
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
- */
-
-if ( !defined( 'MEDIAWIKI' ) ) die();
-
-/**
- * Result printer that prints query results as a tag cloud.
  */
 class SRFTagCloud extends SMWResultPrinter {
 
@@ -29,25 +24,50 @@ class SRFTagCloud extends SMWResultPrinter {
 	}
 
 	public function getResultText( /* SMWQueryResult */ $results, $outputmode ) {
-		return $this->getTagCloud( $this->getTags( $results ) );
+		return $this->getTagCloud( $this->getTags( $results, $outputmode ) );
 	}
 	
-	protected function getTags( SMWQueryResult $results ) {
+	/**
+	 * Returns an array with the tags (keys) and the number of times they occur (values).
+	 * 
+	 * @since 1.5.3
+	 * 
+	 * @param SMWQueryResult $results
+	 * @param $outputmode
+	 * 
+	 * @return array
+	 */
+	protected function getTags( SMWQueryResult $results, $outputmode ) {
 		$tags = array();
 		
 		while ( /* array of SMWResultArray */ $row = $results->getNext() ) { // Objects (pages)
 			for ( $i = 0, $n = count( $row ); $i < $n; $i++ ) { // Properties
 				while ( ( $obj = $row[$i]->getNextObject() ) !== false ) { // Property values
-					if ( $obj->getTypeID() == '_wpg' ) {
-						$images[] = $obj->getTitle(); 
-					}					
+					$value = $obj->getTypeID() == '_wpg' ? $obj->getTitle()->getText() : $obj->getShortText( $outputmode );
+
+					if ( !array_key_exists( $value, $tags ) ) {
+						$tags[$value] = 0;
+					}
+					
+					$tags[$value]++;
 				}					
 			}
 		}
+		
+		return $tags;
 	}
 	
+	/**
+	 * Returns the HTML for the tag cloud.
+	 * 
+	 * @since 1.5.3
+	 * 
+	 * @param array $tags
+	 * 
+	 * @return string
+	 */
 	protected function getTagCloud( array $tags ) {
-		
+		return '';
 	}
 	
 }
