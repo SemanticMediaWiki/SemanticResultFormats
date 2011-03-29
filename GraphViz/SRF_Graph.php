7762509851d4f0635cd4988f9de3c78ac93d82a8
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-03-29 12:53:10 +0000

    cleanup of graph query printer

--- GraphViz/SRF_Graph.php
+++ GraphViz/SRF_Graph.php
@@ -46,92 +46,128 @@ class SRFGraph extends SMWResultPrinter {
 		$this->m_graphColor = array_key_exists( 'graphcolor', $params ) && strtolower( trim( $params['graphcolor'] ) ) == 'yes';
 	}
 	
-	protected function getResultText( $res, $outputmode ) {
+	protected function getResultText( /* SMWQueryResult */ $res, $outputmode ) {
 		$wgGraphVizSettings = new GraphVizSettings;
 		$this->isHTML = true;
 	
 	    $key = 0;
-		// Create text graph
-		$graphInput = '';
+		
 		$legendInput = '';
-
-		$graphInput .= "digraph $this->m_graphName {";
+		
+		$graphInput = "digraph $this->m_graphName {";
 		if ( $this->m_graphSize != '' ) $graphInput .= "size=\"$this->m_graphSize\";";
-		$graphInput .= "rankdir=$this->m_rankdir;";
-	while ( $row = $res->getNext() ) {
-
-			$firstcol = true;
-
-			foreach ( $row as $field ) {
-
-				while ( ( $object = $field->getNextObject() ) !== false ) {
-
-					$text = $object->getShortText( $outputmode );
-
-					if ( $firstcol ) {
-						$firstcolvalue = $object->getShortText( $outputmode );
-
-					}
-
-					if ( $this->m_graphLink == true ) {
-						$nodeLinkTitle = Title::newFromText( $text );
-						$nodeLinkURL = $nodeLinkTitle->getLocalURL();
-
-						$graphInput .= " \"$text\" [URL = \"$nodeLinkURL\"]; ";
-					}
-
-					if ( !$firstcol ) {
-					$graphInput .= " \"$firstcolvalue\" -> \"$text\" ";
-						if ( ( $this->m_graphLabel == true ) || ( $this->m_graphColor == true ) ) {
-							$graphInput .= " [";
-							$req = $field->getPrintRequest();
-							$labelName = $req->getLabel();
-
-							if ( array_search( $labelName, $this->m_labelArray, true ) === false ) {
-								$this->m_labelArray[] = $labelName;
-							}
-								$key = array_search( $labelName, $this->m_labelArray, true );
-								$color = $this->m_graphColors[$key];
-
-							if ( $this->m_graphLabel == true ) {
-								$graphInput .= "label=\"$labelName\"";
-								if ( $this->m_graphColor == true ) $graphInput .= ",fontcolor=$color,";
-							}
-							if ( $this->m_graphColor == true ) {
-
-								$graphInput .= "color=$color";
-							}
-							$graphInput .= "]";
-
-						}
-						$graphInput .= ";";
-
-					}
-				}
-
-				$firstcol = false;
-			}
-
+		$graphInput .= "rankdir=$this->m_rankdir;";		
+		
+		while ( $row = $res->getNext() ) {
+			$graphInput .= $this->getGVForItem( $row, $outputmode );
 		}
-
+		
 		$graphInput .= "}";
+		
 		// Calls renderGraphViz function from MediaWiki GraphViz extension
 		$result = renderGraphviz( $graphInput );
-		if ( ( $this->m_graphLegend == true ) && ( $this->m_graphColor == true ) ) {
+		
+		if ( $this->m_graphLegend && $this->m_graphColor ) {
 			$arrayCount = 0;
 			$result .= "<P>";
+			
 			foreach ( $this->m_labelArray as $m_label ) {
 				$color = $this->m_graphColors[$arrayCount];
 				$result .= "<font color=$color>$color: $m_label </font><br />";
 				$arrayCount = $arrayCount + 1;
 			}
+			
 			$result .= "</P>";
 		}
 		
 		return $result;
 	}
+
+	/**
+	 * Returns the GV for a single subject.
+	 * 
+	 * @since 1.5.4
+	 * 
+	 * @param array $row
+	 * @param $outputmode
+	 * 
+	 * @return string
+	 */
+	protected function getGVForItem( array /* of SMWResultArray */ $row, $outputmode ) {	
+		$segments = array();
+		
+		// Loop throught all fields of the record.
+		foreach ( $row as $i => $resultArray ) {
+
+			// Loop throught all the parts of the field value.
+			while ( ( $object = $resultArray->getNextObject() ) !== false ) {
+				if ( $i == 0 ) {
+					$firstColValue = $object->getShortText( $outputmode );
+					$labelName = $resultArray->getPrintRequest()->getLabel();
+				}
+							
+				$segments[] = $this->getGVForDataValue( $object, $outputmode, $i == 0, $firstColValue, $labelName );
+			}
+		}
+
+		return implode( "\n", $segments );
+	}
 	
-	function getParameters() {
+	/**
+	 * Returns the GV for a single SMWDataValue.
+	 * 
+	 * @since 1.5.4
+	 * 
+	 * @param SMWDataValue $object
+	 * @param $outputmode
+	 * @param boolean $isFirstColumn
+	 * @param string $firstColValue
+	 * @param string $labelName
+	 * 
+	 * @return string
+	 */	
+	protected function getGVForDataValue( SMWDataValue $object, $outputmode, $isFirstColumn, $firstColValue, $labelName ) {
+		$graphInput = '';
+		$text = $object->getShortText( $outputmode );
+
+		if ( $this->m_graphLink == true ) {
+			$nodeLinkTitle = Title::newFromText( $text );
+			$nodeLinkURL = $nodeLinkTitle->getLocalURL();
+
+			$graphInput .= " \"$text\" [URL = \"$nodeLinkURL\"]; ";
+		}
+
+		if ( !$isFirstColumn ) {
+			$graphInput .= " \"$firstColValue\" -> \"$text\" ";
+			
+			if ( $this->m_graphLabel && $this->m_graphColor ) {
+				$graphInput .= ' [';
+
+				if ( array_search( $labelName, $this->m_labelArray, true ) === false ) {
+					$this->m_labelArray[] = $labelName;
+				}
+					$key = array_search( $labelName, $this->m_labelArray, true );
+					$color = $this->m_graphColors[$key];
+
+				if ( $this->m_graphLabel ) {
+					$graphInput .= "label=\"$labelName\"";
+					if ( $this->m_graphColor ) $graphInput .= ",fontcolor=$color,";
+				}
+				if ( $this->m_graphColor ) {
+					$graphInput .= "color=$color";
+				}
+				
+				$graphInput .= ']';
+	
+			}
+			
+			$graphInput .= ';';
+		}
+
+		return $graphInput;
+	}
+	
+	public function getParameters() {
 		return array(
 			array( 'name' => 'graphname', 'type' => 'string', 'description' => wfMsg( 'srf_paramdesc_graphname' ) ),
 			array( 'name' => 'graphsize', 'type' => 'int', 'description' => wfMsg( 'srf_paramdesc_graphsize' ) ),
