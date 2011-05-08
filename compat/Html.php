42b8c78bb47ffebad37523e8aa5a47ae7d0e52a6
Sam Reed <reedy@users.mediawiki.org>
2011-05-08 21:45:52 +0000

    Minor revert of comment change from r87595

--- compat/Html.php
+++ compat/Html.php
@@ -530,7 +530,7 @@ class Html {
 
 	/**
 	 * Convenience function to produce an input element with type=hidden, like
-	 * Html::Hidden.
+	 * Xml::Hidden.
 	 *
 	 * @param $name    string name attribute
 	 * @param $value   string value attribute
