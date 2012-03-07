218dc8fea3aeb18d5278228c9c5da66b043115f1
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2012-03-07 18:34:09 +0000

    follow up to r113170

--- GraphViz/SRF_Process.php
+++ GraphViz/SRF_Process.php
@@ -153,8 +153,9 @@ class SRFProcess extends SMWResultPrinter {
 			/* SMWDataItem */ $subject = $row[0]->getResultSubject(); // get Subject of the Result
 			// creates a new node if $val has type wikipage
 			if ( $subject->getDIType() == SMWDataItem::TYPE_WIKIPAGE ) {
-				/* SMWDataValue */ $dataValue = SMWDataValueFactory::newDataItemValue( $subject, SMWDIProperty::getDataItemClassNameForId( $subject->getDIType() ) );
-				$node = $this->m_process->makeNode( $dataValue->getShortWikiText(), $dataValue->getShortWikiText() );
+				$wikiPageValue = new SMWWikiPageValue( '_wpg' );
+				$wikiPageValue->setDataItem( $subject );
+				$node = $this->m_process->makeNode( $wikiPageValue->getShortWikiText(), $wikiPageValue->getShortWikiText() );
 			}
 
      		//
