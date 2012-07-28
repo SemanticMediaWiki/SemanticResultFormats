ac2830a606fa763a44e62fa638449e43d39d2cdb
mwjames <jamesin.hongkong.1@gmail.com>
2012-07-28 21:46:02 +0900

    Minor gallery JS additions
    - Better modularity (using $.fn)
    - Use galleryID as grouping entity to ensure that only those
    assigned to a gallery group are displayed in the same overlay session
    - Fetch image text from the gallery text element in order to be able to
    display <a> elements in the title as well
    
    Change-Id: Idbbc586e10a0a16bc224f62b16149d2f5c8b11c5

--- formats/gallery/resources/ext.srf.gallery.overlay.js
+++ formats/gallery/resources/ext.srf.gallery.overlay.js
@@ -11,17 +11,15 @@
  *
  * @since: 1.8
  *
- * @release: 0.1
+ * @release: 0.2
  */
 (function( $ ) {
 
-/* Error handling *************************************************************/
 	try { console.log('console ready'); } catch (e) { var console = { log: function () { } }; }
-/* Start javascript ***********************************************************/
 
 	var _this = this;
 
-	// Jeroen's SF image preview code snippset
+	// API image url fetch (see Jeroen's SF image preview)
 	this.getImageURL = function( title , callback ) {
 		$.getJSON(
 			mw.config.get( 'wgScriptPath' ) + '/api.php',
@@ -49,47 +47,53 @@
 		);
 	};
 
+	$.fn.galleryOverlay = function( options ) {
+		var galleryID = this.attr( 'id' ),
+			srfPath = mw.config.get( 'srf.options' ).srfgScriptPath;
+
+		// Loop over all relevant gallery items
+		this.find( '.gallerybox' ).each( function () {
+			var $this   = $( this ),
+				image     = $this.find( 'a.image' ),
+				imageText = $this.find( '.gallerytext p' ).html();
+
+			// Group images
+			image.attr( 'rel', image.has( 'img' ).length ? galleryID : '' );
+
+			// Copy text information for image text display
+			imageText = imageText !== null ? imageText :  image.find( 'img' ).attr( 'alt' );
+			image.attr( 'title', imageText );
+
+			// There should be a better way to find the title object but there isn't
+			title = image.attr( 'href' ).replace(/.+?\File:(.*)$/, "$1" ).replace( "%27", "\'" );
+
+			// Assign image url
+			_this.getImageURL( title ,
+					function( url ) { if ( url === false ) {
+						image.attr( 'href', '' );
+					}	else {
+						image.attr( 'href', url );
+					}
+			} );
+		} );
+
+		// Formatting the title
+		function formatTitle( title, currentArray, currentIndex, currentOpts ) {
+			return '<div class="srf-fancybox-title"><span class="button"><a href="javascript:;" onclick="$.fancybox.close();"><img src=' +  srfPath + '/resources/jquery.fancybox/closelabel.gif' + '></a></span>' + (title && title.length ? '<b>' + title : '' ) + '<span class="count"> (' +  mw.msg( 'srf-gallery-overlay-count', (currentIndex + 1) , currentArray.length ) + ')</span></div>';
+		}
+
+		// Display all images related to a group
+		this.find( "a[rel^=" + galleryID + "]" ).fancybox( {
+			'showCloseButton' : false,
+			'titlePosition'   : 'inside',
+			'titleFormat'     : formatTitle
+		} );
+	}
+
+	// DOM
 	$(document).ready(function() {
-
 		$( ".srf-overlay" ).each(function() {
-			var $this   = $( this );
-			var srfPath = mw.config.get( 'srf.options' ).srfgScriptPath;
-
-			// Loop over all relevant gallery items
-			$this.find( 'a.image' ).each( function () {
-				var $this = $( this );
-
-				// Group images
-				$this.attr( 'rel', $this.has( 'img' ).length ? 'overlay' : '' );
-
-				// Copy text information for image text display
-				$this.attr( 'title', $this.find( 'img' ).attr( 'alt' ) );
-
-				// There should be a better way to find the title object but there isn't
-				title = $this.attr( 'href' ).replace(/.+?\File:(.*)$/, "$1" ).replace( "%27", "\'" );
-
-				// Assign image url
-				_this.getImageURL( title ,
-						function( url ) { if ( url === false ) {
-							$this.attr( 'href', '' );
-						}	else {
-							$this.attr( 'href', url );
-						}
-				} );
-			} );
-
-			// Format title display
-			function formatTitle( title, currentArray, currentIndex, currentOpts ) {
-				return '<div class="srf-fancybox-title"><span class="button"><a href="javascript:;" onclick="$.fancybox.close();"><img src=' +  srfPath + '/resources/jquery.fancybox/closelabel.gif' + '></a></span>' + (title && title.length ? '<b>' + title : '' ) + '<span class="count"> (' +  mw.msg( 'srf-gallery-overlay-count', (currentIndex + 1) , currentArray.length ) + ')</span></div>';
-			}
-
-			// Display all images related to a group
-			$this.find( "a[rel^='overlay']" ).fancybox( {
-				'showCloseButton' : false,
-				'titlePosition'   : 'inside',
-				'titleFormat'     : formatTitle
-			} );
-/* End javascript ************************************************************/
-		} ); // end of initilized $this object
-	} ); // end $(document).ready
+			$( this ).galleryOverlay();
+		} );
+	} );
 } )( window.jQuery, window.mediaWiki );
