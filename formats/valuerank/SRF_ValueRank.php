86b9137e4d38f47b7423aa0392be3a4e9d6b1e51
mwjames <jamesin.hongkong.1@gmail.com>
2012-11-07 03:05:59 +0900

    SRF Value rank add template support and some clean-up
    + add template support
    + add list styles (ul, ol, or none)
    + parameter mincount has been renamed to min
    + add class parameter for additional CSS class assignments
    + adopt SRF 1.8 naming convention
    + docs++
    
    ## Example to generate value ranks as table
    
    Introtemplate
    <includeonly>
    {|
    !Name
    !Rank
    </includeonly>
    
    Template
    <includeonly>
    |-
    |{{{1|}}}
    |{{{rank|}}}
    </includeonly>
    
    Outrotemplate
    <includeonly>
    |}
    </includeonly>
    
    Change-Id: I762cde6a4e54fddf953f9a10dd232f0124ae4812

--- formats/valuerank/SRF_ValueRank.php
+++ formats/valuerank/SRF_ValueRank.php
@@ -4,87 +4,117 @@
  * Result printer that prints query results as a valuerank.
  * In other words, it prints a list of all occuring values, with duplicates removed,
  * together with their occurrence count.
- * 
- * For example, this result set: foo bar baz foo bar bar ohi 
+ *
+ * Build out of Tag Cloud Format by Jeroen De Dauw < jeroendedauw@gmail.com >
+ *
+ * For example, this result set: foo bar baz foo bar bar ohi
  * Will be turned into
  * * bar (3)
  * * foo (2)
  * * baz (1)
  * * ohi (1)
- * 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
  * @since 1.7
- * 
- * @file SRF_ValueRank.php
+ *
+ * @file
  * @ingroup SemanticResultFormats
- * 
- * @licence GNU GPL v3
+ *
+ * @licence GNU GPL v2
  * @author DaSch < dasch@daschmedia.de >
- * build out of Tag Cloud Format by Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author mwjames
  */
 class SRFValueRank extends SMWResultPrinter {
 
-	protected $includeName;
-	protected $minCount;
-	protected $maxTags;
-	
+	/**
+	 * @var array
+	 */
 	protected $tagsHtml = array();
-	
+
+	/**
+	 * @see SMWResultPrinter::getName
+	 *
+	 * @return string
+	 */
 	public function getName() {
 		return wfMessage( 'srf_printername_valuerank' )->text();
 	}
-	
+
 	/**
-	 * @see SMWResultPrinter::handleParameters
-	 * 
+	 * @see SMWResultPrinter::getResultText
+	 *
 	 * @since 1.7
-	 * 
-	 * @param array $params
-	 * @param $outputmode
+	 *
+	 * @param SMWQueryResult $results
+	 * @param $outputMode
+	 *
+	 * @return string
 	 */
-	protected function handleParameters( array $params, $outputmode ) {
-		parent::handleParameters( $params, $outputmode );
-		
-		$this->includeName = $params['includesubject'];
-		$this->minCount = $params['mincount'];
-		$this->maxTags = $params['maxtags'];
+	public function getResultText( SMWQueryResult $results, $outputMode ) {
+
+		// Template support
+		$this->hasTemplates = $this->params['template'] !== '';
+
+		// Prioritize HTML setting
+		$this->isHTML = $this->params['template'] === '';
+
+		$outputMode = SMW_OUTPUT_HTML;
+
+		return $this->getFormatOutput( $this->getValueRank( $this->getResultValues( $results, $outputMode ) ) );
 	}
 
-	public function getResultText( SMWQueryResult $results, $outputmode ) {
-		$this->isHTML = $outputmode == SMW_OUTPUT_HTML;
-		return $this->getVRValueRank( $this->getVRRank( $this->getVRValues( $results, $outputmode ) ) );
-	}
-	
 	/**
 	 * Returns an array with the tags (keys) and the number of times they occur (values).
-	 * 
+	 *
 	 * @since 1.7
-	 * 
+	 *
 	 * @param SMWQueryResult $results
-	 * @param $outputmode
-	 * 
+	 * @param $outputMode
+	 *
 	 * @return array
 	 */
-	protected function getVRValues( SMWQueryResult $results, $outputmode ) {
+	protected function getResultValues( SMWQueryResult $results, $outputMode ) {
 		$tags = array();
-		
-		while ( /* array of SMWResultArray */ $row = $results->getNext() ) { // Objects (pages)
-			for ( $i = 0, $n = count( $row ); $i < $n; $i++ ) { // SMWResultArray for a sinlge property 
-				while ( ( /* SMWDataValue */ $dataValue = $row[$i]->getNextDataValue() ) !== false ) { // Data values
-					
+
+		/**
+		 * @var $row SMWResultArray Objects (pages)
+		 * @var $dataValue SMWDataValue
+		 *
+		 * @return array
+		 */
+		while ( $row = $results->getNext() ) {
+			// SMWResultArray for a sinlge property
+			for ( $i = 0, $n = count( $row ); $i < $n; $i++ ) {
+				while ( ( $dataValue = $row[$i]->getNextDataValue() ) !== false ) {
+
 					$isSubject = $row[$i]->getPrintRequest()->getMode() == SMWPrintRequest::PRINT_THIS;
-					
+
 					// If the main object should not be included, skip it.
-					if ( $i == 0 && !$this->includeName && $isSubject ) {
+					if ( $i == 0 && !$this->params['includesubject'] && $isSubject ) {
 						continue;
 					}
-					
+
 					// Get the HTML for the tag content. Pages are linked, other stuff is just plaintext.
 					if ( $dataValue->getTypeID() == '_wpg' ) {
 						$value = $dataValue->getTitle()->getText();
-						$html = $dataValue->getLongText( $outputmode, $this->getLinker( $isSubject ) );
+						$html = $dataValue->getLongText( $outputMode, $this->getLinker( $isSubject ) );
 					}
 					else {
-						$html = $dataValue->getShortText( $outputmode, $this->getLinker( false ) );
+						$html = $dataValue->getShortText( $outputMode, $this->getLinker( false ) );
 						$value = $html;
 					}
 
@@ -92,69 +122,99 @@ class SRFValueRank extends SMWResultPrinter {
 						$tags[$value] = 0;
 						$this->tagsHtml[$value] = $html; // Store the HTML separetely, so sorting can be done easily.
 					}
-					
+
 					$tags[$value]++;
 				}
 			}
 		}
-		
+
 		foreach ( $tags as $name => $count ) {
-			if ( $count < $this->minCount ) {
+			if ( $count < $this->params['min'] ) {
 				unset( $tags[$name] );
 			}
 		}
 		return $tags;
 	}
-	
+
 	/**
-	 * Determines the sizes of tags.
-	 * This method is based on code from the FolkTagCloud extension by Katharina Wäschle.
-	 * 
+	 * Determine ranks
+	 *
 	 * @since 1.7
-	 * 
+	 *
 	 * @param array $tags
-	 * 
+	 *
 	 * @return array
-	 */	
-	protected function getVRRank( array $tags ) {
+	 */
+	protected function getValueRank( array $tags ) {
 		if ( count( $tags ) == 0 ) {
 			return $tags;
 		}
-		
+
 		arsort( $tags, SORT_NUMERIC );
-		
-		if ( count( $tags ) > $this->maxTags ) {
-			$tags = array_slice( $tags, 0, $this->maxTags, true );
+
+		if ( count( $tags ) > $this->params['maxtags'] ) {
+			$tags = array_slice( $tags, 0, $this->params['maxtags'], true );
 		}
-		
+
 		return $tags;
 	}
-	
+
 	/**
-	 * Returns the HTML for the tag cloud.
-	 * 
-	 * @since 1.7
-	 * 
+	 * Format the output representation
+	 *
+	 * @since 1.8
+	 *
 	 * @param array $tags
-	 * 
+	 *
 	 * @return string
 	 */
-	protected function getVRValueRank( array $tags ) {
+	protected function getFormatOutput( array $tags ) {
 		$htmlTags = array();
 
-		foreach ( $tags as $name => $size ) {
-			$htmlTags[] = Html::rawElement(
-				'li',
-				array( 'style' => "font-size:$size" ),
-				$this->tagsHtml[$name] . '&nbsp;(' . $size . ')'
-			);
+		if ( $this->params['introtemplate'] !== '' && $this->params['template'] !== '' ){
+			$htmlTags[] = "{{" . $this->params['introtemplate'] . "}}";
 		}
-		
+
+		foreach ( $tags as $name => $size ) {
+			if ( $this->params['template'] !== '' ){
+				$htmlTags[] = $this->addTemplateOutput( $name, $size, $rownum );
+			} else {
+				$htmlTags[] = Html::rawElement(
+					( $this->params['liststyle'] === 'none' ? 'span' : 'li' ),
+					array( 'style' => "font-size:$size" ),
+					$this->tagsHtml[$name] . '&nbsp;(' . $size . ')'
+				);
+			}
+		}
+
+		if ( $this->params['outrotemplate'] !== '' && $this->params['template'] !== '' ){
+			$htmlTags[] = "{{" . $this->params['outrotemplate'] . "}}";
+		}
+
 		return Html::rawElement(
-			'ol',
-			array( 'align' => 'left' ),
-			implode( ' ', $htmlTags )
+			( $this->params['liststyle'] === 'none' ? 'div' : $this->params['liststyle'] ),
+			array( 'class' => $this->params['class']  ),
+			implode( '' , $htmlTags )
 		);
+	}
+
+	/**
+	 * Create a template output
+	 *
+	 * @since 1.8
+	 *
+	 * @param $value
+	 * @param $rownum
+	 *
+	 * @return string
+	 */
+	protected function addTemplateOutput( $name, $rank, &$rownum ) {
+		$rownum++;
+		$wikitext  = $this->params['userparam'] ? "|userparam=" . $this->params['userparam'] : '';
+		$wikitext .= "|" . $name;
+		$wikitext .= "|rank=" . $rank;
+		$wikitext .= "|#=$rownum";
+		return '{{' . trim ( $this->params['template'] ) . $wikitext . '}}';
 	}
 
 	/**
@@ -175,7 +235,7 @@ class SRFValueRank extends SMWResultPrinter {
 			'message' => 'srf_paramdesc_includesubject',
 		);
 
-		$params['mincount'] = array(
+		$params['min'] = array(
 			'type' => 'integer',
 			'default' => 1,
 			'message' => 'srf_paramdesc_mincount',
@@ -186,8 +246,38 @@ class SRFValueRank extends SMWResultPrinter {
 			'default' => 1000,
 			'message' => 'srf_paramdesc_maxtags',
 		);
-		
+
+		$params['template'] = array(
+			'message' => 'srf-paramdesc-template',
+			'default' => '',
+		);
+
+		$params['userparam'] = array(
+			'message' => 'srf-paramdesc-userparam',
+			'default' => '',
+		);
+
+		$params['introtemplate'] = array(
+			'message' => 'smw-paramdesc-introtemplate',
+			'default' => '',
+		);
+
+		$params['outrotemplate'] = array(
+			'message' => 'smw-paramdesc-outrotemplate',
+			'default' => '',
+		);
+
+		$params['liststyle'] = array(
+			'message' => 'srf-paramdesc-liststyle',
+			'default' => 'ul',
+			'values' => array( 'ul', 'ol', 'none' ),
+		);
+
+		$params['class'] = array(
+			'message' => 'srf-paramdesc-class',
+			'default' => '',
+		);
+
 		return $params;
 	}
-	
 }
