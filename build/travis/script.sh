da95cbe6640b864b098b7c9565fee3d16d4c3e5e
Jeroen De Dauw <jeroendedauw@gmail.com>
2013-12-02 05:19:36 +0100

    Update script.sh

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -6,7 +6,7 @@ cd ../phase3/extensions/SemanticResultFormats
 
 if [ "$MW-$DBTYPE" == "master-mysql" ]
 then
-	phpunit --coverage-clover ../../extensions/SemanticMediaWiki/build/logs/clover.xml
+	phpunit --coverage-clover ../../extensions/SemanticResultFormats/build/logs/clover.xml
 else
 	phpunit
 fi
