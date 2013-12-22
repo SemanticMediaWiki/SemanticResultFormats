633866c2fde43ff7743a71acb590c5f99ece2c21
Jeroen De Dauw <jeroendedauw@gmail.com>
2013-12-22 06:06:02 +0100

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,14 @@
 <?php
 
+if ( php_sapi_name() !== 'cli' ) {
+	die( 'Not an entry point' );
+}
+
 require_once( __DIR__ . '/evilMediaWikiBootstrap.php' );
 
-echo exec( 'composer update' ) . "\n";
+$pwd = exec( 'pwd' );
+chdir( __DIR__ . '/..' );
+passthru( 'composer update' );
+chdir( $pwd );
 
 require_once( __DIR__ . '/../vendor/autoload.php' );
