2d1d21095887643a1512d85e38773204f68095f6
jeroendedauw <jeroendedauw@gmail.com>
2014-01-03 01:25:03 +0100

    Improve hook registration
    Change-Id: If2e9578af48acfb548cca8dfa05d114b480b58a8

--- SemanticResultFormats.php
+++ SemanticResultFormats.php
@@ -75,11 +75,9 @@ global $wgHooks;
 // Admin Links hook needs to be called in a delayed way so that it
 // will always be called after SMW's Admin Links addition; as of
 // SMW 1.9, SMW delays calling all its hook functions.
-$wgExtensionFunctions[] = 'srffAddAdminLinksHook';
-function srffAddAdminLinksHook() {
-	global $wgHooks;
-	$wgHooks['AdminLinks'][] = 'SRFHooks::addToAdminLinks';
-}
+$wgExtensionFunctions[] = function() {
+	$GLOBALS['wgHooks']['AdminLinks'][] = 'SRFHooks::addToAdminLinks';
+};
 
 $wgHooks['ParserFirstCallInit'][] = 'SRFParserFunctions::registerFunctions';
 $wgHooks['UnitTestsList'][] = 'SRFHooks::registerUnitTests';
