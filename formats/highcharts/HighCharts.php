0b078f8573fa56de73465027e0a124df6215e074
Kim Eik <kim@heldig.org>
2014-02-13 13:50:58 +0100

    Added frequency distribution chart

---
+++ formats/highcharts/HighCharts.php
@@ -0,0 +1,116 @@
+<?php
+namespace SRF;
+use SMWOutputs;
+use SMWQueryResult;
+use SMWResultPrinter;
+
+/**
+ * Class SRFHighCharts
+ * @package SRF
+ * @author Kim Eik
+ */
+class HighCharts extends SMWResultPrinter {
+
+	/**
+	 * @var \SRF\HighCharts\ChartTemplate[]
+	 */
+	protected $graphs = array(
+		'frequency distribution' => 'SRF\HighCharts\FrequencyDistributionChart',
+	);
+
+	function __construct() {
+		foreach ($this->graphs as &$graph) {
+			$graph = new $graph;
+		}
+	}
+
+	/**
+	 * Return serialised results in specified format.
+	 * Implemented by subclasses.
+	 */
+	protected function getResultText( SMWQueryResult $res, $outputmode ) {
+		SMWOutputs::requireResource( 'ext.srf.highcharts');
+		$id = uniqid ('hc');
+		$graph = $this->graphs['frequency distribution'];
+		$graph->setQueryResult($res);
+		$graph->setParameters($this->params);
+		$js = sprintf(
+			'if (window.srfhighcharts === undefined) {window.srfhighcharts = {}};window.srfhighcharts[\'%s\'] = %s;',
+		$id,$graph->getChartJSON());
+
+		$html = \Html::rawElement( 'div', array(
+				'id' => $id,
+				'style' => "min-width:{$this->params['min-width']}px; height:{$this->params['height']}px; margin 0 auto"
+			)
+		);
+		$this->getOutput()->addInlineScript($js);
+		return $html;
+	}
+
+	/**
+	 * @see SMWResultPrinter::getParamDefinitions
+	 *
+	 * @since 1.8
+	 *
+	 * @param $definitions array of IParamDefinition
+	 *
+	 * @return array of IParamDefinition|array
+	 */
+	public function getParamDefinitions( array $definitions ) {
+		$params = parent::getParamDefinitions( $definitions );
+
+		$params['graph-format'] = array(
+			'message' => 'srf-hc-paramdesc-graph',
+			'default' => '',
+			'values' => array('frequency distribution'),
+		);
+
+		$params['yscale'] = array(
+			'message' => 'srf-hc-paramdesc-yscale',
+			'default' => 'normal',
+			'values' => array('normal','log'),
+		);
+
+		$params['xscale'] = array(
+			'message' => 'srf-hc-paramdesc-xscale',
+			'default' => 'normal',
+			'values' => array('normal','log'),
+		);
+
+		$params['ytitle'] = array(
+			'message' => 'srf-hc-paramdesc-ytitle',
+			'default' => 'Y-axis',
+		);
+
+		$params['xtitle'] = array(
+			'message' => 'srf-hc-paramdesc-xtitle',
+			'default' => 'X-axis',
+		);
+
+		$params['title'] = array(
+			'message' => 'srf-hc-paramdesc-title',
+			'default' => 'Title',
+		);
+
+		$params['subtitle'] = array(
+			'message' => 'srf-hc-paramdesc-subtitle',
+			'default' => 'Subtitle',
+		);
+
+		$params['min-width'] = array(
+			'message' => 'srf-hc-paramdesc-minwidth',
+			'default' => '0',
+		);
+
+		$params['height'] = array(
+			'message' => 'srf-hc-paramdesc-height',
+			'default' => '400',
+		);
+
+		$params = array_merge($params,$this->graphs['frequency distribution']->getParameterDefinitions());
+
+		return $params;
+	}
+
+
+}
