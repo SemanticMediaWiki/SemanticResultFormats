b46f840b6ac7a1851bc0dac4f90334831c3e1301
mwjames <jamesin.hongkong.1@gmail.com>
2015-10-04 16:08:24 +0200

    Add `composer phpunit`

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,11 @@ if ( php_sapi_name() !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
-if ( is_readable( $path = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
-	print( "\nUsing SemanticMediaWiki ...\n" );
-} else {
+if ( !is_readable( $path = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
 	die( 'The SemanticMediaWiki test autoloader is not available' );
 }
 
-$autoloader = require $path;
+print sprintf( "\n%-20s%s\n", "Semantic Result Formats: ", SRF_VERSION );
 
+$autoloader = require $path;
 $autoloader->addPsr4( 'SRF\\Tests\\', __DIR__ . '/phpunit' );
