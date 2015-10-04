fd75c017deaacbb748c167e9baef1a566c22a2b2
mwjames <jamesin.hongkong.1@gmail.com>
2015-10-04 16:15:16 +0200

    Travis to use `composer phpunit`

--- build/travis/run-tests.sh
+++ build/travis/run-tests.sh
@@ -4,5 +4,11 @@ set -ex
 BASE_PATH=$(pwd)
 MW_INSTALL_PATH=$BASE_PATH/../mw
 
-cd $MW_INSTALL_PATH/extensions/SemanticResultFormats/tests
-php mw-phpunit-runner.php
+cd $MW_INSTALL_PATH/extensions/SemanticResultFormats
+
+if [ "$TYPE" == "coverage" ]
+then
+	composer phpunit -- --coverage-clover $BASE_PATH/build/coverage.clover
+else
+	composer phpunit
+fi
