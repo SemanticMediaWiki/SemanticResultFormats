9e54ecde3c7f290356a152292f9bece9d66087c8
Stephan Gambke <s7eph4n@gmail.com>
2017-05-28 23:03:49 +0200

    Add integration testing using JSONScript

---
+++ tests/phpunit/Integration/JSONScript/JsonTestCaseScriptRunnerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SRF\Tests\Integration\JSONScript;
+
+use SMW\Tests\Integration\JSONScript\JsonTestCaseScriptRunnerTest as SMWJsonTestCaseScriptRunnerTest;
+
+/**
+ * @see https://github.com/SemanticMediaWiki/SemanticMediaWiki/tree/master/tests#write-integration-tests-using-json-script
+ *
+ * `JsonTestCaseScriptRunner` provisioned by SMW is a base class allowing to use a JSON
+ * format to create test definitions with the objective to compose "real" content
+ * and test integration with MediaWiki, Semantic MediaWiki, and Scribunto.
+ *
+ * The focus is on describing test definitions with its content and specify assertions
+ * to control the expected base line.
+ *
+ * `JsonTestCaseScriptRunner` will handle the tearDown process and ensures that no test
+ * data are leaked into a production system but requires an active DB connection.
+ *
+ * @group SRF
+ * @group SMWExtension
+ *
+ * @license GNU GPL v2+
+ * @since 2.5
+ *
+ * @author mwjames
+ */
+class JsonTestCaseScriptRunnerTest extends SMWJsonTestCaseScriptRunnerTest {
+
+	/**
+	 * @see \SMW\Tests\JsonTestCaseScriptRunner::getTestCaseLocation
+	 * @return string
+	 */
+	protected function getTestCaseLocation() {
+		return __DIR__ . '/TestCases';
+	}
+}
