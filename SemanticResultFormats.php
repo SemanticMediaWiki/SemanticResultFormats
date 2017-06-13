a99b223a6101d1a275d0584c233f2fdcfa442228
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2017-06-13 15:25:05 +0200

    Create SemanticResultFormats.php

--- SemanticResultFormats.php
+++ SemanticResultFormats.php
@@ -51,7 +51,7 @@ class SemanticResultFormats {
 	 */
 	public static function initExtension() {
 
-		define( 'SRF_VERSION', '2.5.0' );
+		define( 'SRF_VERSION', '3.0.0-alpha' );
 
 		// Register the extension
 		$GLOBALS['wgExtensionCredits']['semantic'][] = [
