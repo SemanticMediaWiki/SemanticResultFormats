706a8d13885284808063cc34c327b42ae0716738
mwjames <jamesin.hongkong.1@gmail.com>
2017-10-14 08:51:56 +0000

    Remove deprecated ApiResultPrinter, refs 317 (#319)

---
+++ src/ResourceFormatter.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace SRF;
+
+use Html;
+use SMWOutputs as ResourceManager;
+use SMWQueryResult as QueryResult;
+
+/**
+ * @since 3.0
+ *
+ * @license GNU GPL v2 or later
+ * @author mwjames
+ */
+class ResourceFormatter {
+
+	/**
+	 * @since 3.0
+	 *
+	 * @param array $modules
+	 * @param array $styleModules
+	 */
+	public static function registerResources( array $modules = [], array $styleModules = [] ) {
+
+		foreach ( $modules as $module ) {
+			ResourceManager::requireResource( $module );
+		}
+
+		foreach ( $styleModules as $styleModule ) {
+			ResourceManager::requireStyle( $styleModule );
+		}
+	}
+
+	/**
+	 * @since 3.0
+	 *
+	 * @return string
+	 */
+	public static function session() {
+		return 'smw-' . uniqid();
+	}
+
+	/**
+	 * Convenience method generating a visual placeholder before any
+	 * JS is registered to indicate that resources (JavaScript, CSS)
+	 * are being loaded and once ready ensure to set
+	 * ( '.smw-spinner' ).hide()
+	 *
+	 * @since 3.0
+	 */
+	public static function placeholder() {
+		self::registerResources( [], [ 'ext.smw.style' ] );
+
+		return Html::rawElement(
+			'div',
+			array( 'class' => 'smw-spinner left mw-small-spinner' ),
+			Html::element(
+				'p',
+				array( 'class' => 'text' ),
+				wfMessage( 'smw-livepreview-loading' )->text()
+			)
+		);
+	}
+
+	/**
+	 *
+	 * @since 3.0
+	 *
+	 * @param string $id
+	 * @param array $data
+	 */
+	public static function encode( $id, $data ) {
+		ResourceManager::requireHeadItem(
+			$id,
+			\Skin::makeVariablesScript(
+				[
+					$id => json_encode( $data )
+				]
+			)
+		);
+	}
+
+	/**
+	 * @param QueryResult $queryResult
+	 * @param $outputMode
+	 *
+	 * @return string
+	 */
+	public static function getData( QueryResult $queryResult, $outputMode, $parameters = [] ) {
+
+		// Add parameters that are only known to the specific printer
+		$ask = $queryResult->getQuery()->toArray();
+
+		foreach ( $parameters as $key => $value ) {
+			if ( is_string( $value ) || is_integer( $value ) || is_bool( $value ) ) {
+				$ask['parameters'][$key] = $value;
+			}
+		}
+
+		// Combine all data into one object
+		$data = array(
+			'query' => array(
+				'result' => $queryResult->toArray(),
+				'ask'    => $ask
+			)
+		);
+
+		return $data;
+	}
+
+}
