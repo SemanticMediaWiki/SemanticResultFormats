6f05faecc72fee92cb0f251dc67b1136ccf8d96c
Stephan Gambke <s7eph4n@gmail.com>
2017-11-05 15:57:07 +0100

    Filtered: More performace optimization; Fix unavailable messages
    * Optimize performance on startup
    * Fix unavailable messages for Value filter (wait for page loaded before using messages)

--- formats/filtered/resources/ts/Filtered/View/TableView.ts
+++ formats/filtered/resources/ts/Filtered/View/TableView.ts
@@ -1,2 +1,9 @@
 import { View } from "./View";
-export class TableView extends View {}
+
+export class TableView extends View {
+
+	protected getItemClassName() {
+		return '.filtered-table-item';
+	}
+
+}
