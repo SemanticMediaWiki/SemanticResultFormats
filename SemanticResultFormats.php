b6ffe99e9d6bb5534d8771e03fee605ecc5eb707
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2018-04-04 21:12:45 +0200

    2.5.5 release (#389)
    * Update RELEASE-NOTES.md
    ** Provide release notes for this release
    * Update SemanticResultFormats.php
    ** Bump version for this release
    * Update INSTALL.md
    ** Update release information

--- SemanticResultFormats.php
+++ SemanticResultFormats.php
@@ -51,7 +51,7 @@ class SemanticResultFormats {
 	 */
 	public static function initExtension() {
 
-		define( 'SRF_VERSION', '2.5.5-alpha' );
+		define( 'SRF_VERSION', '2.5.5' );
 
 		// Register the extension
 		$GLOBALS['wgExtensionCredits']['semantic'][] = [
