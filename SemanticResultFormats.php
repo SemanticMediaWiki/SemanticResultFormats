cf62994d18b21f6acb55cead49a7356dbf9e3e49
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2018-09-07 15:08:41 +0200

    Update SemanticResultFormats.php
    * Increment version number

--- SemanticResultFormats.php
+++ SemanticResultFormats.php
@@ -51,7 +51,7 @@ class SemanticResultFormats {
 	 */
 	public static function initExtension() {
 
-		define( 'SRF_VERSION', '2.5.6-alpha' );
+		define( 'SRF_VERSION', '2.5.6' );
 
 		// Register the extension
 		$GLOBALS['wgExtensionCredits']['semantic'][] = [
