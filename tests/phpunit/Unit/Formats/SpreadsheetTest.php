6ef4e0e34eebdeebd3403162ff44fa9d84521be6
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-10-19 01:09:57 +0200

    Remove out of date comments

--- tests/phpunit/Unit/Formats/SpreadsheetTest.php
+++ tests/phpunit/Unit/Formats/SpreadsheetTest.php
@@ -6,39 +6,18 @@ use SMW\Test\QueryPrinterRegistryTestCase;
 use SRF\SpreadsheetPrinter;
 
 /**
- * Tests for the SRF\Excel class.
- *
- * @since 1.9
- *
  * @ingroup SemanticResultFormats
  * @ingroup Test
- *
  * @group SRF
  * @group SMWExtension
  * @group ResultPrinters
- *
- * @author Kim Eik
  */
 class SpreadsheetTest extends QueryPrinterRegistryTestCase {
 
-	/**
-	 * @see QueryPrinterRegistryTestCase::getFormats
-	 *
-	 * @since 1.8
-	 *
-	 * @return array
-	 */
 	public function getFormats() {
 		return [ 'spreadsheet' ];
 	}
 
-	/**
-	 * @see QueryPrinterRegistryTestCase::getClass
-	 *
-	 * @since 1.8
-	 *
-	 * @return string
-	 */
 	public function getClass() {
 		return 'SRF\SpreadsheetPrinter';
 	}
