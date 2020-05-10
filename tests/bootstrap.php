dad5767d7dbeb5b97042e065aeb629a0c973e5d3
mwjames <jamesin.hongkong.1@gmail.com>
2020-05-10 01:26:09 +0000

    Tidy (bootstrap) (#597)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,13 +4,23 @@ if ( PHP_SAPI !== 'cli' && PHP_SAPI !== 'phpdbg' ) {
 	die( 'Not an entry point' );
 }
 
-if ( !is_readable( $path = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
-	die( 'The SemanticMediaWiki test autoloader is not available' );
+error_reporting( E_ALL | E_STRICT );
+date_default_timezone_set( 'UTC' );
+ini_set( 'display_errors', 1 );
+
+if ( !defined( 'SMW_PHPUNIT_AUTOLOADER_FILE' ) || !is_readable( SMW_PHPUNIT_AUTOLOADER_FILE ) ) {
+	die( "\nThe Semantic MediaWiki test autoloader is not available" );
 }
 
-print sprintf( "\n%-20s%s\n", "Semantic Result Formats: ", SRF_VERSION );
+$width = 25;
 
-$autoloader = require $path;
+if ( !defined( 'SMW_PHPUNIT_FIRST_COLUMN_WIDTH' ) ) {
+	define( 'SMW_PHPUNIT_FIRST_COLUMN_WIDTH', $width );
+}
+
+print sprintf( "\n%-{$width}s%s\n", "Semantic Result Formats: ", SRF_VERSION );
+
+$autoLoader = require SMW_PHPUNIT_AUTOLOADER_FILE;
 $autoloader->addPsr4( 'SRF\\Tests\\', __DIR__ . '/phpunit' );
 $autoloader->addPsr4( 'SMW\\Test\\', __DIR__ . '/../../SemanticMediaWiki/tests/phpunit' );
 $autoloader->addPsr4( 'SMW\\Tests\\', __DIR__ . '/../../SemanticMediaWiki/tests/phpunit' );
