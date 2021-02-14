444e3a53d868c1398a5321d175fa8258a8914196
UnknownSkyrimPasserby <f7022961@opayq.com>
2021-02-14 12:52:10 +0100

    Update SemanticResultFormats.i18n.magic.php

--- SemanticResultFormats.i18n.magic.php
+++ SemanticResultFormats.i18n.magic.php
@@ -102,6 +102,12 @@ $magicWords['nl'] = [
 	'calendarenddate' => [ 0, 'einddatumkalender' ],
 ];
 
+/** Polish (Polski) */
+$magicWords['en'] = [
+	'calendarstartdate' => [ 0, 'kalendarzdatapoczątkowa' ],
+	'calendarenddate' => [ 0, 'kalendarzdatakońcowa' ],
+];
+
 /** Quechua (Runa Simi) */
 $magicWords['qu'] = [
 	'calendarstartdate' => [ 0, 'watapiqallarinapunchaw', 'watapiqallariypunchaw' ],
