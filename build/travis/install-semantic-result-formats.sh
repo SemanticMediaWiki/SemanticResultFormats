d584b4fe6d56eb0ee8c9dd4d04328b77719df05f
Alexander Gesinn <alexander.gesinn@gesinn.it>
2021-02-19 18:35:33 +0100

    first attempt to use Travis CI scripts from SemanticMediaWiki/CIScripts

--- build/travis/install-semantic-result-formats.sh
+++ build/travis/install-semantic-result-formats.sh
@@ -4,83 +4,39 @@ set -ex
 BASE_PATH=$(pwd)
 MW_INSTALL_PATH=$BASE_PATH/../mw
 
-function installPHPUnitWithComposer {
-	if [ "$PHPUNIT" != "" ]
-	then
-		composer require 'phpunit/phpunit='$PHPUNIT --update-with-dependencies
-	fi
-}
+## Install
+echo -e "Running MW root composer install build on $TRAVIS_BRANCH \n"
 
-function installSMWWithComposer {
-	if [ "$SMW" != "" ]
-	then
-		composer require "mediawiki/semantic-media-wiki=$SMW" --update-with-dependencies
-	fi
-}
+cd $MW_INSTALL_PATH
 
-# Run Composer installation from the MW root directory
-function installToMediaWikiRoot {
-	echo -e "Running MW root composer install build on $TRAVIS_BRANCH \n"
+composer require mediawiki/semantic-result-formats "dev-master"
 
-	cd $MW_INSTALL_PATH
+cd extensions
+cd SemanticResultFormats
 
-	installPHPUnitWithComposer
-	installSMWWithComposer
+# Pull request number, "false" if it's not a pull request
+# After the install via composer an additional git fetch is carried out to
+# update the repository to make sure that the latest code changes are
+# deployed for testing
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+	git fetch origin +refs/pull/"$TRAVIS_PULL_REQUEST"/merge:
+	git checkout -qf FETCH_HEAD
+else
+	git fetch origin "$TRAVIS_BRANCH"
+	git checkout -qf FETCH_HEAD
+fi
 
-	if [ "$MERMAID" != "" ]
-    then
-    	composer require 'mediawiki/mermaid='$MERMAID --update-with-dependencies
-	fi
+cd ../..
 
-	composer require mediawiki/semantic-result-formats "dev-master"
+# Rebuild the class map for added classes during git fetch
+composer dump-autoload
 
-	cd extensions
-	cd SemanticResultFormats
+## Configure
+cd $MW_INSTALL_PATH
 
-	# Pull request number, "false" if it's not a pull request
-	# After the install via composer an additional git fetch is carried out to
-	# update the repository to make sure that the latest code changes are
-	# deployed for testing
-	if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-	then
-		git fetch origin +refs/pull/"$TRAVIS_PULL_REQUEST"/merge:
-		git checkout -qf FETCH_HEAD
-	else
-		git fetch origin "$TRAVIS_BRANCH"
-		git checkout -qf FETCH_HEAD
-	fi
+echo 'wfLoadExtension( "SemanticResultFormats" );' >> LocalSettings.php
+echo 'define("SMW_PHPUNIT_PULL_VERSION_FROM_GITHUB", true);' >> LocalSettings.php
+echo '$GLOBALS["srfgFormats"][] = "filtered";' >> LocalSettings.php
 
-	cd ../..
-
-	# Rebuild the class map for added classes during git fetch
-	composer dump-autoload
-}
-
-function updateConfiguration {
-
-	cd $MW_INSTALL_PATH
-
-	# SMW#1732
-	echo 'wfLoadExtension( "SemanticMediaWiki" );' >> LocalSettings.php
-
-	if [ "$MERMAID" != "" ]
-    then
-	    echo 'wfLoadExtension( "Mermaid" );' >> LocalSettings.php
-    fi
-
-	echo 'wfLoadExtension( "SemanticResultFormats" );' >> LocalSettings.php
-
-	echo 'define("SMW_PHPUNIT_PULL_VERSION_FROM_GITHUB", true);' >> LocalSettings.php
-
-	echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
-	echo 'ini_set("display_errors", 1);' >> LocalSettings.php
-	echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
-	echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
-	echo "putenv( 'MW_INSTALL_PATH=$(pwd)' );" >> LocalSettings.php
-	echo '$GLOBALS["srfgFormats"][] = "filtered";' >> LocalSettings.php
-
-	php maintenance/update.php --skip-external-dependencies --quick
-}
-
-installToMediaWikiRoot
-updateConfiguration
+php maintenance/update.php --skip-external-dependencies --quick
