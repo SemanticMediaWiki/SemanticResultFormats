ffc788bf9c252d991830d0c21daf9b0c330a64a9
Sebastian Schmid <sebastian.schmid@gesinn.it>
2021-02-23 14:55:33 +0100

    Add unit tests for `valuerank` format (#645)
    fixes #642

---
+++ tests/phpunit/Unit/Formats/ValueRankTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SRF\Tests\Unit\Formats;
+
+use SMW\Test\QueryPrinterRegistryTestCase;
+use SRFValueRank;
+
+/**
+ * Tests for the SRFValueRank class.
+ *
+ * @file
+ * @since 3.2
+ *
+ * @ingroup SemanticResultFormats
+ * @ingroup Test
+ *
+ * @group SRF
+ * @group SMWExtension
+ * @group ResultPrinters
+ *
+ * @licence GNU GPL v2+
+ * @author Sebastian Schmid < sebastian.schmid@geinn.it >
+ */
+class ValueRankTest extends QueryPrinterRegistryTestCase {
+
+	/**
+	 * @see QueryPrinterRegistryTestCase::getFormats
+	 *
+	 * @since 3.2
+	 *
+	 * @return array
+	 */
+	public function getFormats() {
+		return [ 'valuerank' ];
+	}
+
+	/**
+	 * @see QueryPrinterRegistryTestCase::getClass
+	 *
+	 * @since 3.2
+	 *
+	 * @return string
+	 */
+	public function getClass() {
+		return 'SRFValueRank';
+	}
+
+	public function testGetName() {
+
+		$instance = new SRFValueRank(
+			'valuerank'
+		);
+
+		$this->assertInternalType('string', $instance->getName());
+
+	}
+
+}
