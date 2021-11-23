e8ea3bbe6cb124f66119c859665478c1b070d927
Markus <92986684+gesinn-it-wam@users.noreply.github.com>
2021-11-23 11:22:56 +0100

    Fix filtered format for UCA collations (#686)
    * Added failing test for #568 (filtered format breaks for UCA collations)
    * Fix filtered format for UCA collations
    
    The sort key provided in the UCA case is not to be interpreted as string but as a byte array. To make it JSON serializable, convert it to hexadecimal representation as intended for sorting.
    
    * Removed MW-1.31 from CI matrix (according to #682)

---
+++ tests/phpunit/Unit/Filtered/ResultItemTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace SRF\Tests\Filtered;
+
+use Collation;
+use SMW\MediaWiki\Collator;
+use SMW\Query\PrintRequest;
+use SMW\Query\Result\ResultArray;
+use SMWDataValue;
+use SMWDIWikiPage;
+use SRF\Filtered\Filtered;
+use SRF\Filtered\ResultItem;
+
+/**
+ * @covers \SRF\Filtered\ResultItem
+ * @group semantic-result-formats
+ *
+ * @license GNU GPL v2+
+ * @since 4.0.0
+ *
+ * @author gesinn-it-wam
+ */
+class ResultItemTest extends \PHPUnit_Framework_TestCase {
+
+	public function testArray_representation_is_JSON_serializable_for_UCA_collation_Issue568() {
+		// arrange
+		$sortKey = ( new Collator( Collation::factory( "uca-de-u-kn" ) ) )->getSortKey( 'A' );
+
+		$resultArray = $this->createStub( ResultArray::class );
+
+		$dataItem = $this->createStub( SMWDIWikiPage::class );
+		$dataItem->method( 'getSortKey' )->willReturn( $sortKey );
+		$dataValue = $this->createStub( SMWDataValue::class );
+		$dataValue->method( 'getDataItem' )->willReturn( $dataItem );
+		$dataValue->method( 'getShortWikiText' )->willReturn( null );
+		$resultArray->method( 'getNextDataValue' )->willReturnOnConsecutiveCalls( $dataValue );
+
+		$printRequest = $this->createStub( PrintRequest::class );
+		$printRequest->method( 'getHash' )->willReturn( null );
+		$resultArray->method( 'getPrintRequest' )->willReturn( $printRequest );
+
+		$queryPrinter = new Filtered( null );
+		$instance = new ResultItem( [ $resultArray ], $queryPrinter );
+
+		// act
+		$representation = $instance->getArrayRepresentation();
+
+		// assert
+		$this->assertNotFalse( json_encode( $representation ) );
+	}
+}
