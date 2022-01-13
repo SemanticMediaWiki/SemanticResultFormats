e7b89690d1d26cacbce5e96285bc4854e1d088aa
Mark A. Hershberger <mah@nichework.com>
2022-01-12 19:41:50 -0500

    Fix DocComment sniff

--- formats/graphviz/SRF_Process.php
+++ formats/graphviz/SRF_Process.php
@@ -17,7 +17,7 @@
  *
  *   You should have received a copy of the GNU General Public License
  *   along with Process Printer. If not, see <http://www.gnu.org/licenses/>.
- *******************************************************************************/
+ */
 
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
