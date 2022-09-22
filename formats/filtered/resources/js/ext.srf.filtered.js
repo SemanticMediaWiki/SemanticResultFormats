bed66e00f625d92adf0c2fc8b20c48a71a1b5bf9
thomas-topway-it <topway.it@mail.com>
2022-09-22 13:25:00 +0400

    fix tabs

--- formats/filtered/resources/js/ext.srf.filtered.js
+++ formats/filtered/resources/js/ext.srf.filtered.js
@@ -942,21 +942,21 @@ var ViewSelector = /** @class */ (function () {
         this.controller = controller;
     }
     ViewSelector.prototype.init = function () {
-        var _this = this;
-        if (this.viewIDs.length > 1) {
-			this.viewIDs.forEach(function (id) {
-				_this.target.on('click', '.' + id, { 'target': id, 'controller': _this.controller }, ViewSelector.onSelectorSelected); 
+	var _this = this;
+	if (this.viewIDs.length > 1) {
+		this.viewIDs.forEach(function (id) {
+			_this.target.on('click', '.' + id, { 'target': id, 'controller': _this.controller }, ViewSelector.onSelectorSelected); 
 
-				_this.target.on('keydown', '.' + id, { 'target': id, 'controller': _this.controller }, function( e ) {
-					if ( e.keyCode == 13 ) {	
-						ViewSelector.onSelectorSelected(e);
-					}
-				});
-
+			_this.target.on('keydown', '.' + id, { 'target': id, 'controller': _this.controller }, function( e ) {
+				if ( e.keyCode == 13 ) {	
+					ViewSelector.onSelectorSelected(e);
+				}
 			});
-            this.target.children().first().addClass('selected');
-            this.target.show();
-        }
+
+		});
+		this.target.children().first().addClass('selected');
+		this.target.show();
+	}
     };
     ViewSelector.onSelectorSelected = function (event) {
         event.data.controller.onViewSelected(event.data.target);
