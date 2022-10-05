989cf1322706993a1fcecec1dc77e20feba070f7
thomas-topway-it <topway.it@mail.com>
2022-10-05 12:51:01 +0400

    fix resource

--- formats/slideshow/SRF_SlideShow.php
+++ formats/slideshow/SRF_SlideShow.php
@@ -89,7 +89,9 @@ class SRFSlideShow extends SMWResultPrinter {
 		SMWOutputs::requireResource( 'ext.srf.slideshow' );
 
 		if ( $this->params['nav controls'] ) {
-			SMWOutputs::requireResource( 'jquery.ui.slider' );
+			// *** this resource has been removed from recent Mediawiki versions
+			// SMWOutputs::requireResource( 'jquery.ui.slider' );
+			SMWOutputs::requireResource( 'jquery.ui' );
 		}
 
 		return Html::element(
