8fb2004423e6ffa5c33c47a5f053dc6f693c32d8
thomas-topway-it <topway.it@mail.com>
2023-02-14 02:02:46 +0400

    datatables native options

--- formats/datatables/resources/ext.srf.formats.datatables.js
+++ formats/datatables/resources/ext.srf.formats.datatables.js
@@ -6,10 +6,8 @@
  */
 
 /* global jQuery, mediaWiki, mw */
-( function ( $, mw ) {
-
-	'use strict';
-
+(function ($, mw) {
+	"use strict";
 
 	/**
 	 * @since 3.0
@@ -18,19 +16,18 @@
 	 * @param container {Object}
 	 * @param api {Object}
 	 */
-	var Query = function ( container, api ) {
-
+	var Query = function (container, api) {
 		this.VERSION = "3.0";
 
 		this.container = container;
 		this.mwApi = api;
 
-		this.title = mw.config.get( 'wgPageName' );
-		this.data = container.data( 'query' );
+		this.title = mw.config.get("wgPageName");
+		this.data = container.data("query");
 		this.query = this.data.query;
 
 		this.cmd = this.data.cmd;
-		this.control = container.find( '#deferred-control' ).data( 'control' );
+		this.control = container.find("#deferred-control").data("control");
 
 		this.limit = this.data.limit;
 		this.offset = this.data.offset;
@@ -41,23 +38,22 @@
 
 		this.max = this.data.max;
 		this.step = 5;
-		this.postfix = '';
+		this.postfix = "";
 
-
-// console.log("this.query",this.query)
+		// console.log("this.query",this.query)
 
 		// Ensure to have a limit, offset parameter for queries that use
 		// the default setting
-		if ( this.query.indexOf( "|limit=" ) == -1 ) {
-			this.query = this.query + '|limit=' + this.limit;
+		if (this.query.indexOf("|limit=") == -1) {
+			this.query = this.query + "|limit=" + this.limit;
 		}
 
-		if ( this.query.indexOf( "|offset=" ) == -1 ) {
-			this.query = this.query + '|offset=' + this.offset;
+		if (this.query.indexOf("|offset=") == -1) {
+			this.query = this.query + "|offset=" + this.offset;
 		}
 
-		if ( this.query.indexOf( "|default=" ) == -1 ) {
-			this.query = this.query + '|default=' + mw.msg( 'smw_result_noresults' );
+		if (this.query.indexOf("|default=") == -1) {
+			this.query = this.query + "|default=" + mw.msg("smw_result_noresults");
 		}
 	};
 
@@ -66,27 +62,24 @@
 	 *
 	 * @since 3.0
 	 */
-	Query.prototype.doApiRequest = function() {
+	Query.prototype.doApiRequest = function () {
+		return;
 
 		var self = this,
-			noTrace = '';
+			noTrace = "";
 
 		// Replace limit, offset with altered values
-		var query = self.query.replace(
-			'limit=' + self.limit,
-			'limit=' + self.rangeLimit
-		).replace(
-			'offset=' + self.offset,
-			'offset=' + self.rangeOffset
-		);
+		var query = self.query
+			.replace("limit=" + self.limit, "limit=" + self.rangeLimit)
+			.replace("offset=" + self.offset, "offset=" + self.rangeOffset);
 
 		// In case the query was altered from its original request, signal
 		// to the QueryDependencyLinksStore to disable any tracking
-		if ( self.query !== query ) {
-			noTrace = '|@notrace';
-		};
+		if (self.query !== query) {
+			noTrace = "|@notrace";
+		}
 
-/*
+		/*
 console.log( {
 			action: "parse",
 			title: self.title,
@@ -98,54 +91,60 @@ console.log( {
 		// API notes "modules: Gives the ResourceLoader modules used on the page.
 		// Either jsconfigvars or encodedjsconfigvars must be requested jointly
 		// with modules. 1.24+"
-		self.mwApi.post( {
-			action: "parse",
-			title: self.title,
-			contentmodel: 'wikitext',
-			prop: 'text|modules|jsconfigvars',
-			text: '{{#' + self.cmd + ':' +  query + noTrace + '}}'
-		} ).done( function( data ) {
+		self.mwApi
+			.post({
+				action: "parse",
+				title: self.title,
+				contentmodel: "wikitext",
+				prop: "text|modules|jsconfigvars",
+				text: "{{#" + self.cmd + ":" + query + noTrace + "}}",
+			})
+			.done(function (data) {
+				console.log("data", data);
+				if (self.init === true) {
+					self.initControls();
+					self.replaceOutput("");
+				}
 
-console.log("data", data)
-			if ( self.init === true ) {
-				self.initControls();
-				self.replaceOutput( '' );
-			};
+				// Remove any comments retrieved from the API parse
+				var text = data.parse.text["*"].replace(/<!--[\S\s]*?-->/gm, "");
 
-			// Remove any comments retrieved from the API parse
-			var text = data.parse.text['*'].replace(/<!--[\S\s]*?-->/gm, '' );
+				// Remove any remaining placeholder loading classes
+				if (self.control !== "") {
+					text = text.replace("smw-loading-image-dots", "");
+				}
 
-			// Remove any remaining placeholder loading classes
-			if ( self.control !== '' ) {
-				text = text.replace( 'smw-loading-image-dots', '' );
-			}
+				// Remove any <p> element to avoid line breakages
+				if (self.cmd === "show") {
+					text = text.replace(/(?:^<p[^>]*>)|(?:<\/p>$)/gim, "");
+				}
 
-			// Remove any <p> element to avoid line breakages
-			if ( self.cmd === 'show' ) {
-				text = text.replace( /(?:^<p[^>]*>)|(?:<\/p>$)/img, '' );
-			}
+				self.replaceOutput(text, "", data.parse.modules);
+			})
+			.fail(function (code, failure) {
+				var error = code + ": " + failure.textStatus;
 
-			self.replaceOutput( text, '', data.parse.modules );
+				if (
+					failure.hasOwnProperty("exception") &&
+					failure.hasOwnProperty("xhr")
+				) {
+					error = failure.xhr.responseText;
+				} else if (
+					failure.hasOwnProperty("error") &&
+					failure.error.hasOwnProperty("info")
+				) {
+					error = failure.error.info;
+				}
 
-		} ).fail ( function( code, failure ) {
-			var error =  code + ': ' + failure.textStatus;
-
-			if ( failure.hasOwnProperty( 'exception' ) && failure.hasOwnProperty( 'xhr' ) ) {
-				error = failure.xhr.responseText;
-			} else if ( failure.hasOwnProperty( 'error' ) && failure.error.hasOwnProperty( 'info' ) ) {
-				error = failure.error.info;
-			}
-
-			self.container.find( '#deferred-control' ).replaceWith( "<div id='deferred-control'></div>" );
-			self.container.find( '.irs' ).hide();
-			self.replaceOutput( error, "smw-callout smw-callout-error" );
-		} );
+				self.container
+					.find("#deferred-control")
+					.replaceWith("<div id='deferred-control'></div>");
+				self.container.find(".irs").hide();
+				self.replaceOutput(error, "smw-callout smw-callout-error");
+			});
 	};
 
-
-
 	var dataTable = {
-
 		/**
 		 * Adds the initial sort/order from the #ask request that is available as
 		 * `data-column-sort` attribute with something like:
@@ -173,45 +172,48 @@ console.log("data", data)
 		 *
 		 * @param {Object} context
 		 */
-		initColumnSort: function ( context ) {
-
-			var column = context.data( 'column-sort' );
+		initColumnSort: function (context) {
+			var column = context.data("column-sort");
 			var order = [];
 
 			// SMW allows descending and ascending but those are unknown to DataTables
 			var orderMap = {
-				'descending' : 'desc',
-				'ascending': 'asc',
-				'asc': 'asc',
-				'desc': 'desc'
+				descending: "desc",
+				ascending: "asc",
+				asc: "asc",
+				desc: "desc",
 			};
 
 			// In case of a transposed table, don't try to match a column or its order
-			if ( column === undefined || !column.hasOwnProperty( 'sort' ) || column.sort.length === 0 || context.attr( 'data-transpose' ) ) {
+			if (
+				column === undefined ||
+				!column.hasOwnProperty("sort") ||
+				column.sort.length === 0 ||
+				context.attr("data-transpose")
+			) {
 				return;
-			};
+			}
 
 			// https://datatables.net/reference/api/order()
 			// [1, 'asc'], [2, 'desc']
-			$.map( column.sort, function( val, i ) {
-
-				if ( val === '' ) {
+			$.map(column.sort, function (val, i) {
+				if (val === "") {
 					i = 0;
-				};
+				}
 
-				if ( $.inArray( val, column.list ) < 0 ) {
-					return
-				};
+				if ($.inArray(val, column.list) < 0) {
+					return;
+				}
 
-				order.push( [
-					$.inArray( val, column.list ), // Find matchable index from the list
-					column.order[i] === undefined ? 'asc' : orderMap[column.order[i]]
-				] );
-			} );
+				order.push([
+					$.inArray(val, column.list), // Find matchable index from the list
+					column.order[i] === undefined ? "asc" : orderMap[column.order[i]],
+				]);
+			});
 
-			if ( order.length > 0 ) {
-				context.data( 'order', order );
-			};
+			if (order.length > 0) {
+				context.data("order", order);
+			}
 		},
 
 		/**
@@ -222,24 +224,23 @@ console.log("data", data)
 		 *
 		 * @param {Object} context
 		 */
-		addHeader: function ( context ) {
-
+		addHeader: function (context) {
 			// Copy the thead to a position the DataTable plug-in can transform
 			// and display
-			if ( context.find( 'thead' ).length === 0 ) {
-				var head = context.find( 'tbody tr' );
-				context.prepend( '<thead>' + head.html() + '</thead>' );
+			if (context.find("thead").length === 0) {
+				var head = context.find("tbody tr");
+				context.prepend("<thead>" + head.html() + "</thead>");
 				head.eq(0).remove();
 
 				// In case of a transposed, turn any td into a th
-				context.find( 'thead td' ).wrapInner( '<th />' ).contents().unwrap();
+				context.find("thead td").wrapInner("<th />").contents().unwrap();
 			}
 
 			// Ensure that any link in the header stops the propagation of the
 			// click sorting event
-			context.find( 'thead tr a' ).on( 'click.sorting', function ( event ) {
+			context.find("thead tr a").on("click.sorting", function (event) {
 				event.stopPropagation();
-			} );
+			});
 		},
 
 		/**
@@ -250,31 +251,39 @@ console.log("data", data)
 		 *
 		 * @param {Object} context
 		 */
-		addFooter: function ( context ) {
-
+		addFooter: function (context) {
 			// As a transposed table, move the footer column to the bottom
 			// and remove any footer-cell from the table matrix to
 			// ensure a proper formatted table
-			if ( context.data( 'transpose' ) === 1 && context.find( 'tbody .sortbottom' ).length === 1 ) {
-				var footer = context.find( 'tbody .sortbottom' );
-				context.append( '<tfoot><tr><td colspan=' + footer.index() + '>' + footer.html() + '</td></tr></tfoot>' );
+			if (
+				context.data("transpose") === 1 &&
+				context.find("tbody .sortbottom").length === 1
+			) {
+				var footer = context.find("tbody .sortbottom");
+				context.append(
+					"<tfoot><tr><td colspan=" +
+						footer.index() +
+						">" +
+						footer.html() +
+						"</td></tr></tfoot>"
+				);
 				footer.eq(0).remove();
 
 				// Remove remaining footer cells to avoid an uneven table
-				context.find( 'tbody .footer-cell' ).each( function() {
-					$( this ).remove();
-				} );
-			};
+				context.find("tbody .footer-cell").each(function () {
+					$(this).remove();
+				});
+			}
 
 			// Copy the tbody to a position the DataTable plug-in can transform
 			// and display
-			if ( context.find( 'tbody .smwfooter' ).length == 1 ) {
-				var footer = context.find( 'tbody .smwfooter' );
-				context.append( '<tfoot>' + footer.html() + '</tfoot>' );
+			if (context.find("tbody .smwfooter").length == 1) {
+				var footer = context.find("tbody .smwfooter");
+				context.append("<tfoot>" + footer.html() + "</tfoot>");
 				footer.eq(0).remove();
 			}
 
-			context.find( '.sortbottom' ).addClass( 'plainlinks' );
+			context.find(".sortbottom").addClass("plainlinks");
 		},
 
 		/**
@@ -282,78 +291,74 @@ console.log("data", data)
 		 *
 		 * @param {Object} context
 		 */
-		addToolbarExportLinks: function ( context ) {
-
-			var toolbar = context.parent().find( '.smw-datatable-toolbar' ),
-				query = context.data( 'query' ),
+		addToolbarExportLinks: function (context) {
+			var toolbar = context.parent().find(".smw-datatable-toolbar"),
+				query = context.data("query"),
 				exportFormats = {
-				'JSON': {
-					'format': 'json',
-					'searchlabel': 'JSON',
-					'type': 'simple',
-					'prettyprint': true,
-					'unescape': true
-				},
-				'CSV': {
-					'format': 'csv',
-					'searchlabel': 'CSV'
-				},
-				'RSS': {
-					'format': 'rss',
-					'searchlabel': 'RSS'
-				},
-				'RDF': {
-					'format': 'rdf',
-					'searchlabel': 'RDF'
-				}
-			}
+					JSON: {
+						format: "json",
+						searchlabel: "JSON",
+						type: "simple",
+						prettyprint: true,
+						unescape: true,
+					},
+					CSV: {
+						format: "csv",
+						searchlabel: "CSV",
+					},
+					RSS: {
+						format: "rss",
+						searchlabel: "RSS",
+					},
+					RDF: {
+						format: "rdf",
+						searchlabel: "RDF",
+					},
+				};
 
-			if ( !query instanceof Object || query === undefined ) {
+			if (!query instanceof Object || query === undefined) {
 				return;
 			}
 
-			var items = '';
+			var items = "";
 
-			Object.keys( exportFormats ).forEach( function( key ) {
-
+			Object.keys(exportFormats).forEach(function (key) {
 				// https://stackoverflow.com/questions/122102/what-is-the-most-efficient-way-to-deep-clone-an-object-in-javascript
 				var conf = exportFormats[key],
-					parameters = $.extend( {}, query.parameters );
+					parameters = $.extend({}, query.parameters);
 
 				// Modify the default query with that of the configuration
-				Object.keys( conf ).forEach(function( k ) {
+				Object.keys(conf).forEach(function (k) {
 					parameters[k] = conf[k];
-				} );
+				});
 
-				var q = new smw.query(
-					query.printouts,
-					parameters,
-					query.conditions
-				);
+				var q = new smw.query(query.printouts, parameters, query.conditions);
 
-				q.setLinkAttributes( {
-					'title': key
-				} );
+				q.setLinkAttributes({
+					title: key,
+				});
 
-				if ( key === 'RDF' ) {
+				if (key === "RDF") {
 					items += '<li class="divider"></li>';
-				};
+				}
 
-				items += '<li class="action">' +  q.getLink( key ) + '</li>';
-			} );
+				items += '<li class="action">' + q.getLink(key) + "</li>";
+			});
 
 			toolbar.append(
 				'<span class="smw-dropdown">' +
-				'<button>' + mw.msg( 'smw-format-datatable-toolbar-export' ) + '</button>' +
-				'<label><input type="checkbox">' +
-				'<ul class="smw-dropdown-menu">' +
-				items +
-				'</ul></label></span>'
+					"<button>" +
+					mw.msg("smw-format-datatable-toolbar-export") +
+					"</button>" +
+					'<label><input type="checkbox">' +
+					'<ul class="smw-dropdown-menu">' +
+					items +
+					"</ul></label></span>"
 			);
 
-			toolbar.find( '.action a' ).on( 'click', function( e ) {
-				toolbar.find( '.smw-dropdown input' ).prop( 'checked', false );
-			} );
+			toolbar.find(".action a").on("click", function (e) {
+				toolbar.find(".smw-dropdown input").prop("checked", false);
+			});
 		},
 
 		/**
@@ -361,48 +366,86 @@ console.log("data", data)
 		 *
 		 * @param {Object} context
 		 */
-		attach: function ( context ) {
-
+		attach: function (context) {
 			var self = this;
 			context.show();
 
 			// Remove any class that may interfere due to some external JS or CSS
-			context.removeClass( 'jquery-tablesorter' );
-			context.removeClass( 'sortable' );
-			context.removeClass( 'is-disabled' );
-			context.removeClass( 'wikitable' );
+			context.removeClass("jquery-tablesorter");
+			context.removeClass("sortable");
+			context.removeClass("is-disabled");
+			context.removeClass("wikitable");
 
 			// DataTables default display class
-			context.addClass( 'display' );
+			context.addClass("display");
 
 			//mw.loader.using( 'onoi.dataTables' ).done( function () {
 
-				self.initColumnSort( context );
+			self.initColumnSort(context);
 
-				// MediaWiki table output is missing some standard formatting hence
-				// add a footer and header
-				self.addFooter( context );
-				self.addHeader( context );
+			// MediaWiki table output is missing some standard formatting hence
+			// add a footer and header
+			self.addFooter(context);
+			self.addHeader(context);
 
-				// https://datatables.net/manual/tech-notes/3
-				// Ensure the object initialization only happens once
-				if ( $.fn.dataTable.isDataTable( context ) ) {
-					return;
+			// https://datatables.net/manual/tech-notes/3
+			// Ensure the object initialization only happens once
+			if ($.fn.dataTable.isDataTable(context)) {
+				return;
+			}
+
+			console.log("context.data", context.data());
+
+			var options = context.data("datatables");
+
+			var arrayTypes = [
+				"LengthMenu",
+				"buttons",
+				// ...
+			];
+
+			for (var i in options) {
+				if (arrayTypes.indexOf(i) !== -1) {
+					options[i] = options[i]
+						.split(",")
+						.map((x) => x.trim())
+						.filter((x) => x !== "");
 				}
+			}
 
+			console.log("options", options);
 
-				var table = context.DataTable( {
+			if (options.buttons.length && options.dom.indexOf("B") === -1) {
+				options.dom = "B" + options.dom;
+			}
 
-			order: 1,
-			pageLength: 20,
+			/*
+            deferRender:    true,
+            scrollY:        200,
+            scrollCollapse: true,
+            scroller:       true
+*/
+			var table = context.DataTable(
+				$.extend(
+					{
+						//	order: 1,
+						//	pageLength: 20,
+						// https://datatables.net/reference/option/dom
+						//	dom: 'B<"srf-datatable-left"f><"srf-datatable-right"l>rtip',
+						//	lengthMenu: [10, 20, 50, 100, 200],
+						//buttons: [
+						//           'copy', 'csv', 'excel', 'pdf', 'print'
+						//       ]
+					},
+					options
+				)
+			);
 
-			// https://datatables.net/reference/option/dom
-			dom: '<"srf-datatable-left"f><"srf-datatable-right"l>rtip',
-			lengthMenu: [ 10, 20, 50, 100, 200 ],
+			console.log("options", options);
 
-				} );
+			return;
 
-/*
+			/*
 
 					dom: 'l<"smw-datatable-toolbar float-right">frtip',
 					searchHighlight: true,
@@ -435,23 +478,22 @@ console.log("data", data)
 						}
 					}
 */
-				// Remove accented characters from the search input
-				context.parent().find( 'input' ).on( "keyup", function () {
-					table.search(
-						$.fn.DataTable.ext.type.search.string( $.trim( this.value ) )
-					).draw();
-				} );
+			// Remove accented characters from the search input
+			context
+				.parent()
+				.find("input")
+				.on("keyup", function () {
+					table
+						.search($.fn.DataTable.ext.type.search.string($.trim(this.value)))
+						.draw();
+				});
 
-				self.addToolbarExportLinks( context );
+			self.addToolbarExportLinks(context);
 
-				// Fire to instantiate the tooltip after the DT has been generated
-				mw.hook( 'smw.tooltip' ).fire( context );
+			// Fire to instantiate the tooltip after the DT has been generated
+			mw.hook("smw.tooltip").fire(context);
 
-
-			var q = new Query(
-				context,
-				new mw.Api()
-			);
+			var q = new Query(context, new mw.Api());
 
 			q.doApiRequest();
 
@@ -466,29 +508,26 @@ console.log("data", data)
 		 *
 		 * @param {Object} context
 		 */
-		init: function ( context ) {
+		init: function (context) {
+			context.removeClass("is-disabled");
+			context.removeClass("smw-flex-center");
 
-			context.removeClass( 'is-disabled' );
-			context.removeClass( 'smw-flex-center' );
+			context.css("background-color", "transparent");
+			context.css("height", "");
+			context.find(".smw-overlay-spinner").hide();
 
-			context.css( "background-color", "transparent" );
-			context.css( "height", "" );
-			context.find( '.smw-overlay-spinner' ).hide();
+			context.find(".datatable").css("opacity", "1");
+			context.removeClass("smw-extra-margin");
 
-			context.find( '.datatable' ).css( "opacity", "1" );
-			context.removeClass( 'smw-extra-margin' );
-
-			context.find( '.smw-datatable' ).removeClass( 'smw-extra-margin' );
-			this.attach( context.find( '.datatable' ) );
-		}
+			context.find(".smw-datatable").removeClass("smw-extra-margin");
+			this.attach(context.find(".datatable"));
+		},
 	};
 
-	$( document ).ready( function() {
+	$(document).ready(function () {
+		$(".smw-datatable").each(function () {
+			dataTable.init($(this));
+		});
+	});
+})(jQuery, mediaWiki);
 
-		$( '.smw-datatable' ).each( function() {
-			dataTable.init( $( this ) );
-		} );
-
-	} );
-
-}( jQuery, mediaWiki ) );
