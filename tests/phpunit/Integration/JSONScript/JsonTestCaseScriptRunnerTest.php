ce7fe52e91924752471626b149c0a7ef5917b8b7
paladox <paladox@users.noreply.github.com>
2024-12-29 11:18:55 +0000

    Update JsonTestCaseScriptRunnerTest.php

--- tests/phpunit/Integration/JSONScript/JsonTestCaseScriptRunnerTest.php
+++ tests/phpunit/Integration/JSONScript/JsonTestCaseScriptRunnerTest.php
@@ -14,6 +14,7 @@ use SMW\Tests\Integration\JSONScript\JSONScriptTestCaseRunnerTest;
  * @group SRF
  * @group SMWExtension
  * @group Database
+ * @group Parser
  *
  * @license GPL-2.0-or-later
  * @since 2.5
