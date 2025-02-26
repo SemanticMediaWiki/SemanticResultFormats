62f97657bd3ef034d49b9bf3f6ecda64d35a124e
alistair3149 <alistair3149@users.noreply.github.com>
2025-02-25 22:47:03 -0500

    Update SRF_Boilerplate.php

--- formats/boilerplate/SRF_Boilerplate.php
+++ formats/boilerplate/SRF_Boilerplate.php
@@ -1,8 +1,8 @@
 <?php
 
 use SMW\Query\QueryResult;
-use SMW\Query\ResultPrinters\ResultPrinter;
 use SMW\Query\Result\ResultArray;
+use SMW\Query\ResultPrinters\ResultPrinter;
 
 /**
  * Boilerplate query printer
