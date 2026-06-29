192990223b9ecbce77d905b2fd848e7f75107b05
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2026-06-29 21:59:08 +0200

    test(vcard,bibtex,outline): add unit tests for while/if-condition branches
    Cover the previously untested AssignmentInCondition paths in
    BibTexFileExportPrinter, vCardFileExportPrinter, ListTreeBuilder, and
    TemplateBuilder — enabling safe removal of phpcs:ignore suppressions.
    
    Also extract vCardFileExportPrinter::getPageTimestamp() as a protected
    method so getVCardContent() is unit-testable without MediaWikiServices.

--- tests/phpunit/Unit/Outline/ListTreeBuilderTest.php
+++ tests/phpunit/Unit/Outline/ListTreeBuilderTest.php
@@ -2,7 +2,13 @@
 
 namespace SRF\Tests\Outline;
 
+use SMW\Query\PrintRequest;
+use SMW\Query\Result\ResultArray;
+use SMWDataItem;
+use SMWDataValue;
+use SMWWikiPageValue;
 use SRF\Outline\ListTreeBuilder;
+use SRF\Outline\OutlineItem;
 use SRF\Outline\OutlineTree;
 
 /**
@@ -31,9 +37,74 @@ class ListTreeBuilderTest extends \PHPUnit\Framework\TestCase {
 		$instance = new ListTreeBuilder( $params );
 
 		$this->assertIsString(
-
 			$instance->build( new OutlineTree() )
 		);
+	}
+
+	public function testBuildWithItemIncludesValueInOutput() {
+		$params = [
+			'outlineproperties' => [ 'OutlineProp' ],
+			'link' => 'none',
+			'showHeaders' => false,
+		];
+
+		$dataItem = $this->createMock( SMWDataItem::class );
+		$dataItem->method( 'getDIType' )->willReturn( SMWDataItem::TYPE_BLOB );
+
+		$dataValue = $this->createMock( SMWDataValue::class );
+		$dataValue->method( 'getDataItem' )->willReturn( $dataItem );
+		$dataValue->method( 'getShortText' )->willReturn( 'TestValue' );
+
+		$printRequest = $this->createMock( PrintRequest::class );
+		$printRequest->method( 'getText' )->willReturn( 'SomeProperty' );
+		$printRequest->method( 'getLabel' )->willReturn( 'SomeProperty' );
+		$printRequest->method( 'isMode' )->willReturn( false );
+
+		$resultArray = $this->createMock( ResultArray::class );
+		$resultArray->method( 'getPrintRequest' )->willReturn( $printRequest );
+		$resultArray->method( 'reset' )->willReturn( null );
+		$resultArray->method( 'getNextDataValue' )->willReturnOnConsecutiveCalls( $dataValue, false );
+
+		$item = new OutlineItem( [ $resultArray ] );
+		$tree = new OutlineTree( [ $item ] );
+
+		$instance = new ListTreeBuilder( $params );
+		$output = $instance->build( $tree );
+
+		$this->assertStringContainsString( 'TestValue', $output );
+	}
+
+	public function testBuildWithWikiPageItemUsesDisplayTitleAsCaption() {
+		$params = [
+			'outlineproperties' => [ 'OutlineProp' ],
+			'link' => 'none',
+			'showHeaders' => false,
+		];
+
+		$dataItem = $this->createMock( SMWDataItem::class );
+		$dataItem->method( 'getDIType' )->willReturn( SMWDataItem::TYPE_WIKIPAGE );
+
+		$dataValue = $this->createMock( SMWWikiPageValue::class );
+		$dataValue->method( 'getDataItem' )->willReturn( $dataItem );
+		$dataValue->method( 'getDisplayTitle' )->willReturn( 'My Display Title' );
+		$dataValue->expects( $this->once() )->method( 'setCaption' )->with( 'My Display Title' );
+		$dataValue->method( 'getShortText' )->willReturn( 'My Display Title' );
+
+		$printRequest = $this->createMock( PrintRequest::class );
+		$printRequest->method( 'getText' )->willReturn( 'SomeProperty' );
+		$printRequest->method( 'getLabel' )->willReturn( 'SomeProperty' );
+		$printRequest->method( 'isMode' )->willReturn( false );
+
+		$resultArray = $this->createMock( ResultArray::class );
+		$resultArray->method( 'getPrintRequest' )->willReturn( $printRequest );
+		$resultArray->method( 'reset' )->willReturn( null );
+		$resultArray->method( 'getNextDataValue' )->willReturnOnConsecutiveCalls( $dataValue, false );
+
+		$item = new OutlineItem( [ $resultArray ] );
+		$tree = new OutlineTree( [ $item ] );
+
+		$instance = new ListTreeBuilder( $params );
+		$instance->build( $tree );
 	}
 
 }
