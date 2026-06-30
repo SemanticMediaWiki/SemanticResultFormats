12076c6d32e3627b0eb785d34575047e24110fea
thomas-topway-it <topway.it@mail.com>
2026-06-30 16:36:25 +0400

    add suggestion

--- tests/qunit/formats/ext.srf.formats.tagcloud.test.js
+++ tests/qunit/formats/ext.srf.formats.tagcloud.test.js
@@ -36,6 +36,7 @@
 		'<div id="test1" class="srf-tags"><ul>' +
 		'<li><a href="/test1">Test1</a></li>' +
 		'<li><a href="/test2">Test2</a></li>' +
+		'<li>TextOnly</li>' +
 		'</ul></div></div></div>', '#qunit-fixture' );
 
 	/**
@@ -134,7 +135,7 @@
 	 * @since: 1.9
 	 */
 	QUnit.test( 'sphere', function ( assert ) {
-		assert.expect( 1 );
+		assert.expect( 3 );
 		var tagcloud = new srf.formats.tagcloud();
 
 		context.find( '.srf-container' ).data( {
@@ -149,6 +150,11 @@
 		const done = assert.async();
 		mw.loader.using( 'ext.jquery.tagcanvas', function() {
 			tagcloud.sphere( context );
+
+			// @see https://github.com/SemanticMediaWiki/SemanticResultFormats/pull/1050#discussion_r3488225643
+			assert.equal( context.find( 'li:not(:has(a))' ).length, 0, 'all li elements have an anchor after sphere()' );
+			assert.equal( context.find( 'li:has(a > a)' ).length, 0, 'no li is double-wrapped' );
+
 			assert.ok( context.find( 'canvas' ), 'canvas element was found' );
 			done();
 		} );
