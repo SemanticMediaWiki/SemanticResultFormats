SemanticMediaWiki/SemanticResultFormats

commit fb1a1e0ee0fc061b6275f2165e99a97a33e70c0c

refactor(array,hash): move from formats/ to src/ and migrate to SRF\ namespace
Mirrors the calendar/math refactor pattern. Moves SRFArray and SRFHash
from formats/array/ into src/ArrayFormat/ under the SRF\ArrayFormat
namespace, renaming the classes to ArrayFormat and HashFormat respectively.

.phan/baseline.php
@@ -76,8 +76,8 @@ return [
         'formats/Gantt/src/GanttTask.php' => ['MediaWikiNoEmptyIfDefined', 'PhanTypeMismatchForeach'],
         'formats/JitGraph/SRF_JitGraph.php' => ['PhanPluginUseReturnValueInternalKnown', 'PhanPossiblyUndeclaredVariable', 'PhanRedundantCondition', 'PhanTypeMismatchArgumentNullableInternal', 'PhanUndeclaredClass', 'PhanUndeclaredClassMethod', 'PhanUndeclaredExtendedClass', 'PhanUndeclaredProperty', 'PhanUndeclaredTypeParameter'],
         'formats/Prolog/PrologPrinter.php' => ['PhanCompatibleUnionType', 'PhanUndeclaredClass', 'PhanUndeclaredClassInstanceof', 'PhanUndeclaredClassMethod', 'PhanUndeclaredConstant', 'PhanUndeclaredExtendedClass', 'PhanUndeclaredMethod', 'PhanUndeclaredProperty', 'PhanUndeclaredTypeParameter'],
-        'formats/array/SRF_Array.php' => ['MediaWikiNoEmptyIfDefined', 'PhanPossiblyUndeclaredVariable', 'PhanTypeMismatchArgumentNullable', 'PhanUndeclaredClass', 'PhanUndeclaredClassConstant', 'PhanUndeclaredClassInstanceof', 'PhanUndeclaredClassMethod', 'PhanUndeclaredConstant', 'PhanUndeclaredExtendedClass', 'PhanUndeclaredMethod', 'PhanUndeclaredProperty', 'PhanUndeclaredTypeParameter'],
-        'formats/array/SRF_Hash.php' => ['PhanUndeclaredClassConstant', 'PhanUndeclaredClassMethod', 'PhanUndeclaredTypeParameter'],
+        'src/ArrayFormat/ArrayFormat.php' => ['MediaWikiNoEmptyIfDefined', 'PhanPossiblyUndeclaredVariable', 'PhanTypeMismatchArgumentNullable', 'PhanUndeclaredClass', 'PhanUndeclaredClassConstant', 'PhanUndeclaredClassInstanceof', 'PhanUndeclaredClassMethod', 'PhanUndeclaredConstant', 'PhanUndeclaredExtendedClass', 'PhanUndeclaredMethod', 'PhanUndeclaredProperty', 'PhanUndeclaredTypeParameter'],
+        'src/ArrayFormat/HashFormat.php' => ['PhanUndeclaredClassConstant', 'PhanUndeclaredClassMethod', 'PhanUndeclaredTypeParameter'],
         'formats/boilerplate/SRF_Boilerplate.php' => ['PhanTypeMismatchArgumentProbablyReal', 'PhanUndeclaredClass', 'PhanUndeclaredClassConstant', 'PhanUndeclaredClassMethod', 'PhanUndeclaredExtendedClass', 'PhanUndeclaredProperty', 'PhanUndeclaredTypeParameter'],
         'src/Calendar/EventCalendar.php' => ['PhanPluginDuplicateConditionalTernaryDuplication', 'PhanTypeMismatchDimAssignment', 'PhanUndeclaredClass', 'PhanUndeclaredExtendedClass', 'PhanUndeclaredMethod', 'PhanUndeclaredProperty', 'PhanUndeclaredTypeParameter'],
         'src/Calendar/HistoricalDate.php' => ['PhanTypeInvalidUnaryOperandIncOrDec'],

tests/phpunit/Unit/Formats/SRFArrayTest.php
@@ -5,17 +5,17 @@ namespace SRF\Tests\Unit\Formats;
 use PHPUnit\Framework\TestCase;
 use SMW\Query\QueryResult;
 use SMW\Query\Result\ResultArray;
-use SRFArray;
+use SRF\ArrayFormat\ArrayFormat;
 
 /**
- * Unit tests for SRFArray domain logic (separator composition, gap-hiding,
+ * Unit tests for ArrayFormat domain logic (separator composition, gap-hiding,
  * header rendering, and initializeCfgValue fallback).
  *
  * Uses an anonymous subclass to:
  *  - widen all tested protected methods to public (PHP allows this)
  *  - stub infrastructure methods so no MediaWiki parser bootstrap is required
  *
- * @covers SRFArray
+ * @covers \SRF\ArrayFormat\ArrayFormat
  *
  * @group SRF
  * @group SMWExtension
@@ -26,15 +26,15 @@ use SRFArray;
 class SRFArrayTest extends TestCase {
 
 	/**
-	 * Returns an anonymous SRFArray subclass with:
+	 * Returns an anonymous ArrayFormat subclass with:
 	 *  - all tested protected methods widened to public
 	 *  - separator properties pre-set to known test values
 	 *  - infrastructure methods (getCfgSepText, createArray) stubbed
 	 *
 	 * @param array $overrides Optional property overrides, e.g. ['mHideRecordGaps' => true]
 	 */
-	private function newInstance( array $overrides = [] ): SRFArray {
-		$instance = new class( 'array' ) extends SRFArray {
+	private function newInstance( array $overrides = [] ): ArrayFormat {
+		$instance = new class( 'array' ) extends ArrayFormat {
 
 			/** Stub: return scalar values directly; null for anything else. */
 			protected function getCfgSepText( $obj ) {
@@ -139,7 +139,7 @@ class SRFArrayTest extends TestCase {
 	protected function setUp(): void {
 		parent::setUp();
 		// Reset the static separator cache so tests are independent of execution order.
-		$ref = new \ReflectionProperty( SRFArray::class, 'mDefaultSeps' );
+		$ref = new \ReflectionProperty( ArrayFormat::class, 'mDefaultSeps' );
 		$ref->setAccessible( true );
 		$ref->setValue( null, [] );
 	}

SemanticResultFormats.php
@@ -142,8 +142,8 @@ class SemanticResultFormats {
 			'gallery' => 'SRF\Gallery',
 			'tagcloud' => 'SRF\TagCloud',
 			'valuerank' => 'SRFValueRank',
-			'array' => 'SRFArray',
-			'hash' => 'SRFHash',
+			'array' => 'SRF\ArrayFormat\ArrayFormat',
+			'hash' => 'SRF\ArrayFormat\HashFormat',
 			'd3chart' => 'SRFD3Chart',
 			'tree' => 'SRF\Formats\Tree\TreeResultPrinter',
 			'ultree' => 'SRF\Formats\Tree\TreeResultPrinter',

src/ArrayFormat/ArrayFormat.php
@@ -1,4 +1,17 @@
 <?php
+
+declare( strict_types=1 );
+
+namespace SRF\ArrayFormat;
+
+use MediaWiki\MediaWikiServices;
+use SMW\DataValueFactory;
+use SMW\Query\QueryResult;
+use SMW\Query\Result\ResultArray;
+use SMW\Query\ResultPrinters\ResultPrinter;
+use SMWQuery;
+use SMWRecordValue;
+
 /**
  * Query format for arrays with features for Extensions 'Arrays' and 'HashTables'
  *
@@ -6,22 +19,12 @@
  * @ingroup SemanticResultFormats
  * @author Daniel Werner < danweetz@web.de >
  *
- * Doesn't require 'Arrays' nor 'HashTables' exytensions but has additional features
+ * Doesn't require 'Arrays' nor 'HashTables' extensions but has additional features
  * ('name' parameter in either result format) if they are available.
  *
  * Arrays 2.0+ and HashTables 1.0+ are recommended but not necessary.
  */
-
-use MediaWiki\MediaWikiServices;
-use SMW\DataValueFactory;
-use SMW\Query\QueryResult;
-use SMW\Query\Result\ResultArray;
-use SMW\Query\ResultPrinters\ResultPrinter;
-
-/**
- * Array format
- */
-class SRFArray extends ResultPrinter {
+class ArrayFormat extends ResultPrinter {
 
 	protected static $mDefaultSeps = [];
 	protected $mSep;
@@ -35,9 +38,7 @@ class SRFArray extends ResultPrinter {
 	protected $mHideRecordGaps = false;
 	protected $mHidePropertyGaps = false;
 
-	/**
-	 * @var bool true if 'mainlabel' parameter is set to '-'
-	 */
+	/** @var bool true if 'mainlabel' parameter is set to '-' */
 	protected $mMainLabelHack = false;
 
 	public function __construct( $format, $inline = true ) {
@@ -57,44 +58,21 @@ class SRFArray extends ResultPrinter {
 		return wfMessage( 'srf_printername_' . $this->mFormat )->text();
 	}
 
-	/**
-	 * By overwriting this function, we disable default searchlabel handling?
-	 * public function getResult( QueryResult $results, array $params, $outputmode ) {
-	 * $this->handleParameters( $params, $outputmode );
-	 * return $this->getResultText( $results, $outputmode );
-	 * }
-	 */
 	protected function getResultText( QueryResult $res, $outputmode ) {
-		/*
-		 * @todo
-		 * labels of requested properties could define default values. Seems not possible at the moment because
-		 * \SMW\Query\PrintRequest::getLable() always returns the property name even if no specific label is defined.
-		 */
-
 		$perPage_items = [];
 
-		// for each page:
 		$row = $res->getNext();
 		while ( $row !== false ) {
 			$perProperty_items = [];
 
-			/**
-			 * first field is always the page title, except, mainlabel is set to '-'
-			 *
-			 * @todo Is there some other way to check the data value directly for being the
-			 *		  page title or not? SMWs behavior could change on mainlabel handling...
-			 */
 			$isPageTitle = !$this->mMainLabelHack;
 
-			// for each property on that page:
-			// $row is array(), $field of type ResultArray
 			foreach ( $row as $field ) {
 				$manyValue_items = [];
 				$isMissingProperty = false;
 
 				$manyValues = $field->getContent();
 
-				// If property is not set (has no value) on a page:
 				if ( empty( $manyValues ) ) {
 					$delivery = $this->deliverMissingProperty( $field );
 					$manyValue_items = $this->fillDeliveryArray( $manyValue_items, $delivery );
@@ -106,7 +84,6 @@ class SRFArray extends ResultPrinter {
 						$value_items = [];
 						$isRecord = false;
 
-						// handle page Title:
 						if ( $isPageTitle ) {
 							if ( !$this->mShowPageTitles ) {
 								$isPageTitle = false;
@@ -118,7 +95,6 @@ class SRFArray extends ResultPrinter {
 							);
 						} elseif ( $obj instanceof SMWRecordValue ) {
 							$recordItems = $obj->getDataItems();
-							// walk all single values of the record set:
 							foreach ( $recordItems as $dataItem ) {
 								$recordField = $dataItem !== null ? DataValueFactory::getInstance(
 								)->newDataValueByItem( $dataItem, null ) : null;
@@ -129,10 +105,10 @@ class SRFArray extends ResultPrinter {
 							}
 							$isRecord = true;
 						} else {
-												$value_items = $this->fillDeliveryArray(
-												$value_items,
-												$this->deliverSingleValue( $obj, $this->mLinkOthers )
-													);
+							$value_items = $this->fillDeliveryArray(
+								$value_items,
+								$this->deliverSingleValue( $obj, $this->mLinkOthers )
+							);
 						}
 						$delivery = $this->deliverSingleManyValuesData( $value_items, $isRecord, $isPageTitle );
 						$manyValue_items = $this->fillDeliveryArray( $manyValue_items, $delivery );
@@ -146,7 +122,6 @@ class SRFArray extends ResultPrinter {
 					$field
 				);
 				$perProperty_items = $this->fillDeliveryArray( $perProperty_items, $delivery );
-				// next one could be record or normal value
 				$isPageTitle = false;
 			}
 			$delivery = $this->deliverPageProperties( $perProperty_items );
@@ -154,17 +129,10 @@ class SRFArray extends ResultPrinter {
 			$row = $res->getNext();
 		}
 
-		$output = $this->deliverQueryResultPages( $perPage_items );
-
-		return $output;
+		return $this->deliverQueryResultPages( $perPage_items );
 	}
 
-	/**
-	 * Method fillDeliveryArray
-	 *
-	 */
 	protected function fillDeliveryArray( $array = [], $value = null ) {
-		// don't create any empty entries
 		if ( $value !== null ) {
 			$array[] = $value;
 		}
@@ -186,33 +154,19 @@ class SRFArray extends ResultPrinter {
 	}
 
 	protected function deliverSingleValue( $value, $link = false ) {
-		// return trim( $value->getShortWikiText( $link ) );
 		return trim(
-			Sanitizer::decodeCharReferences( $value->getShortWikiText( $link ) )
+			\Sanitizer::decodeCharReferences( $value->getShortWikiText( $link ) )
 		);
 	}
 
-	/**
-	 * Method deliverMissingProperty
-	 *
-	 * @param ResultArray $field
-	 */
 	protected function deliverMissingProperty( ResultArray $field ) {
 		if ( $this->mHidePropertyGaps ) {
 			return null;
 		} else {
 			return '';
 		}
-		/** @ToDo: System for Default values?... * */
 	}
 
-	/**
-	 * Method deliverSingleManyValuesData
-	 *
-	 * @param $value_items
-	 * @param $containsRecord
-	 * @param $isPageTitle
-	 */
 	protected function deliverSingleManyValuesData( $value_items, $containsRecord, $isPageTitle ) {
 		if ( empty( $value_items ) ) {
 			return null;
@@ -227,7 +181,6 @@ class SRFArray extends ResultPrinter {
 
 		$text = implode( $this->mManySep, $manyValue_items );
 
-		// if property names should be displayed and this is not the page titles value:
 		if ( $this->mShowHeaders !== SMW_HEADERS_HIDE && !$isPageTitle ) {
 			$linker = $this->mShowHeaders === SMW_HEADERS_PLAIN ? null : $this->mLinker;
 			$text = $data->getPrintRequest()->getText( SMW_OUTPUT_WIKI, $linker ) . $this->mHeaderSep . $text;
@@ -244,7 +197,6 @@ class SRFArray extends ResultPrinter {
 
 	protected function deliverQueryResultPages( $perPage_items ) {
 		if ( $this->mArrayName !== null ) {
-			// create Array
 			$this->createArray( $perPage_items );
 			return '';
 		} else {
@@ -252,27 +204,18 @@ class SRFArray extends ResultPrinter {
 		}
 	}
 
-	/**
-	 * Helper function to create a new Array within 'Arrays' extension. Takes care of different versions
-	 * as well as the old 'ArrayExtension'.
-	 */
 	protected function createArray( $array ) {
 		global $wgArrayExtension;
 
 		$arrayId = $this->mArrayName;
 
 		if ( defined( 'ExtArrays::VERSION' ) ) {
-			// 'Arrays' extension 2+
-			/** ToDo: is there a way to get the actual parser which has started the query? */
 			$parser = MediaWikiServices::getInstance()->getParser();
 			ExtArrays::get( $parser )->createArray( $arrayId, $array );
 			return true;
 		}
 
-		// compatbility to 'ArrayExtension' extension before 2.0:
-
 		if ( !isset( $wgArrayExtension ) ) {
-			// Hash extension is not installed in this wiki
 			return false;
 		}
 		$version = null;
@@ -282,10 +225,8 @@ class SRFArray extends ResultPrinter {
 			$version = ExtArrayExtension::VERSION;
 		}
 		if ( $version !== null && version_compare( $version, '1.3.2', '>=' ) ) {
-			// ArrayExtension 1.3.2+
 			$wgArrayExtension->createArray( $arrayId, $array );
 		} else {
-			// dirty way
 			$wgArrayExtension->mArrays[trim( $arrayId )] = $array;
 		}
 		return true;
@@ -301,10 +242,7 @@ class SRFArray extends ResultPrinter {
 		if ( !isset( $cache ) ) {
 			$cache = $this->getCfgSepText( $dfltVal );
 			if ( $cache === null ) {
-				// cache can't be initialized, probably function-reference in user config
-				// but format is not used in inline context, use fallback in this case:
 				global $wgSrfgArraySepTextualFallbacks;
-				// Default to empty string
 				$cache = $wgSrfgArraySepTextualFallbacks[$dfltCacheKey] ?? '';
 			}
 		}
@@ -313,47 +251,34 @@ class SRFArray extends ResultPrinter {
 
 	protected function getCfgSepText( $obj ) {
 		if ( is_array( $obj ) ) {
-			// invalid definition:
 			if ( !array_key_exists( 0, $obj ) ) {
 				return null;
 			}
 
-			// check for config-defined arguments to pass to the page before processing it:
 			if ( array_key_exists( 'args', $obj ) && is_array( $obj['args'] ) ) {
 				$params = $obj['args'];
 			} else {
 				$params = [];
 			}
 
-			// create title of page whose text should be used as separator:
-			$obj = Title::newFromText( $obj[0], ( array_key_exists( 1, $obj ) ? $obj[1] : NS_MAIN ) );
+			$obj = \Title::newFromText( $obj[0], ( array_key_exists( 1, $obj ) ? $obj[1] : NS_MAIN ) );
 		}
-		if ( $obj instanceof Title ) {
-			$article = new Article( $obj );
-		} elseif ( $obj instanceof Article ) {
+		if ( $obj instanceof \Title ) {
+			$article = new \Article( $obj );
+		} elseif ( $obj instanceof \Article ) {
 			$article = $obj;
 		} else {
 			return $obj;
 		}
 
-		/*
-		 * Feature to use page value as separator only works if Parser::parse() is running!
-		 * That's not the case on semantic search special page for example!
-		 */
-		// can't use $this->mInline here since SMW 1.6.2 had a bug setting it to false in most cases!
 		$parser = MediaWikiServices::getInstance()->getParser();
 		if ( $parser->getOptions() === null ) {
-			// if( ! $this->mInline ) {
 			return null;
 		}
 
-		/*
-		 * parse page as if it were included like a template. Never use Parser::recursiveTagParse() or similar
-		 * for this since it would call hooks we don't want to call and won't return wiki text for inclusion!
-		 */
 		$frame = $parser->getPreprocessor()->newCustomFrame( $params );
-		$content = $article->getContent( Revision::RAW )->getNativeData();
-		$text = $parser->preprocessToDom( $content, Parser::PTD_FOR_INCLUSION );
+		$content = $article->getContent( \Revision::RAW )->getNativeData();
+		$text = $parser->preprocessToDom( $content, \Parser::PTD_FOR_INCLUSION );
 		$text = trim( $frame->expand( $text ) );
 
 		return $text;
@@ -364,31 +289,19 @@ class SRFArray extends ResultPrinter {
 		$this->applyArrayParameters( $params );
 	}
 
-	/**
-	 * Applies SRFArray-specific parameters to instance state.
-	 * Extracted from handleParameters() so subclasses and tests can invoke
-	 * the array-specific logic independently of the parent MW bootstrap.
-	 */
 	protected function applyArrayParameters( array $params ): void {
-		// separators:
 		$this->mSep = $params['sep'];
 		$this->mPropSep = $params['propsep'];
 		$this->mManySep = $params['manysep'];
 		$this->mRecordSep = $params['recordsep'];
 		$this->mHeaderSep = $params['headersep'];
 
-		// only use this in inline mode, if text is given. Since SMW 1.6.2 '' is given, so if
-		// we wouldn't check, we would always end up with an array instead of visible output
 		if ( $params['name'] !== false && ( $this->mInline || trim( $params['name'] ) !== '' ) ) {
 			$this->mArrayName = trim( $params['name'] );
 			$this->createArray( [] );
-			// create empty array in case we get no result so we won't have an undefined array in the end.
 		}
 
-		// if mainlabel set to '-', this will cause the titles not to appear, so make sure we catch this!
 		$this->mMainLabelHack = trim( $params['mainlabel'] ) === '-';
-
-		// whether or not to display the page title:
 		$this->mShowPageTitles = strtolower( $params['titles'] ) !== 'hide';
 
 		switch ( strtolower( $params['hidegaps'] ) ) {
@@ -418,25 +331,12 @@ class SRFArray extends ResultPrinter {
 		}
 	}
 
-	/**
-	 * @see ResultPrinter::getParamDefinitions
-	 *
-	 * @since 1.8
-	 *
-	 * @param $definitions array of IParamDefinition
-	 *
-	 * @return array of IParamDefinition|array
-	 */
 	public function getParamDefinitions( array $definitions ): array {
 		$params = parent::getParamDefinitions( $definitions );
-
-		### adjusted basic SMW params: ###
 
 		$definitions['limit']->setDefault( $GLOBALS['smwgQMaxInlineLimit'] );
 		$definitions['link']->setDefault( 'none' );
 		$definitions['headers']->setDefault( 'hide' );
-
-		### new params: ###
 
 		$params['titles'] = [
 			'message' => 'srf_paramdesc_pagetitle',
@@ -457,7 +357,6 @@ class SRFArray extends ResultPrinter {
 			'manipulatedefault' => false,
 		];
 
-		// separators (default values are defined in the following globals:)
 		global $srfgArraySep, $srfgArrayPropSep, $srfgArrayManySep, $srfgArrayRecordSep, $srfgArrayHeaderSep;
 
 		$params['sep'] = [

src/ArrayFormat/HashFormat.php
@@ -1,27 +1,29 @@
 <?php
 
+declare( strict_types=1 );
+
+namespace SRF\ArrayFormat;
+
 use MediaWiki\MediaWikiServices;
 
 /**
- * Query format for arrays with features for Extensions 'Arrays' and 'HashTables'
+ * Query format for hash tables with features for Extension 'HashTables'
  *
  * @file
  * @ingroup SemanticResultFormats
  * @author Daniel Werner < danweetz@web.de >
  *
- * Doesn't require 'Arrays' nor 'HashTables' exytensions but has additional features
- * ('name' parameter in either result format) if they are available.
+ * Doesn't require 'HashTables' extension but has additional features
+ * ('name' parameter) if it is available.
  *
- * Arrays 2.0+ and HashTables 1.0+ are recommended but not necessary.
+ * HashTables 1.0+ is recommended but not necessary.
  */
-class SRFHash extends SRFArray {
+class HashFormat extends ArrayFormat {
 
 	protected $mLastPageTitle;
 
 	protected function deliverPageTitle( $value, $link = false ) {
-		// remember the page title
 		$this->mLastPageTitle = $this->deliverSingleValue( $value, $link );
-		// don't add page title into property list
 		return null;
 	}
 
@@ -35,7 +37,6 @@ class SRFHash extends SRFArray {
 	protected function deliverQueryResultPages( $perPage_items ) {
 		$hash = [];
 		foreach ( $perPage_items as $page ) {
-			// name of page as key, Properties as value
 			$hash[$page[0]] = $page[1];
 		}
 		return parent::deliverQueryResultPages( $hash );
@@ -50,19 +51,14 @@ class SRFHash extends SRFArray {
 			$version = ExtHashTables::VERSION;
 		}
 		if ( $version !== null && version_compare( $version, '0.999', '>=' ) ) {
-			// Version 1.0+, doesn't use $wgHashTables anymore
-			/** ToDo: is there a way to get the actual parser which has started the query? */
 			$parser = MediaWikiServices::getInstance()->getParser();
 			ExtHashTables::get( $parser )->createHash( $hashId, $hash );
 			return true;
 		} elseif ( !isset( $wgHashTables ) ) {
-			// Hash extension is not installed in this wiki
 			return false;
 		} elseif ( $version !== null && version_compare( $version, '0.6', '>=' ) ) {
-			// HashTables 0.6 to 1.0
 			$wgHashTables->createHash( $hashId, $hash );
 		} else {
-			// old HashTables, dirty way
 			$wgHashTables->mHashTables[trim( $hashId )] = $hash;
 		}
 		return true;
@@ -70,22 +66,11 @@ class SRFHash extends SRFArray {
 
 	protected function applyArrayParameters( array $params ): void {
 		parent::applyArrayParameters( $params );
-		// Page title is always the hash key — ignore the 'titles' param.
 		$this->mShowPageTitles = true;
 	}
 
-	/**
-	 * @see \SMW\Query\ResultPrinters\ResultPrinter::getParamDefinitions
-	 *
-	 * @since 1.8
-	 *
-	 * @param IParamDefinition[] $definitions
-	 *
-	 * @return array of IParamDefinition|array
-	 */
 	public function getParamDefinitions( array $definitions ): array {
 		$params = parent::getParamDefinitions( $definitions );
-		// page title is Hash key, otherwise, just use Array format!
 		unset( $params['titles'] );
 		$params['name']['message'] = 'srf_paramdesc_hashname';
 

tests/phpunit/Unit/Formats/ArrayTest.php
@@ -41,7 +41,7 @@ class ArrayTest extends QueryPrinterRegistryTestCase {
 	 * @return string
 	 */
 	public function getClass() {
-		return '\SRFArray';
+		return 'SRF\ArrayFormat\ArrayFormat';
 	}
 
 }

tests/phpunit/Unit/Formats/SRFHashTest.php
@@ -3,17 +3,17 @@
 namespace SRF\Tests\Unit\Formats;
 
 use PHPUnit\Framework\TestCase;
-use SRFArray;
-use SRFHash;
+use SRF\ArrayFormat\ArrayFormat;
+use SRF\ArrayFormat\HashFormat;
 
 /**
- * Unit tests for SRFHash domain logic.
+ * Unit tests for HashFormat domain logic.
  *
  * Uses an anonymous subclass to:
  *  - widen all tested protected methods to public
  *  - stub infrastructure methods so no MediaWiki parser or extension bootstrap is required
  *
- * @covers SRFHash
+ * @covers \SRF\ArrayFormat\HashFormat
  *
  * @group SRF
  * @group SMWExtension
@@ -24,12 +24,12 @@ use SRFHash;
 class SRFHashTest extends TestCase {
 
 	/**
-	 * Returns an anonymous SRFHash subclass with infrastructure stubbed out.
+	 * Returns an anonymous HashFormat subclass with infrastructure stubbed out.
 	 *
 	 * @param array $overrides Optional property overrides, e.g. ['mPropSep' => '|']
 	 */
-	private function newInstance( array $overrides = [] ): SRFHash {
-		$instance = new class( 'hash' ) extends SRFHash {
+	private function newInstance( array $overrides = [] ): HashFormat {
+		$instance = new class( 'hash' ) extends HashFormat {
 
 			/** Stub: return scalars directly; null for anything else. */
 			protected function getCfgSepText( $obj ) {
@@ -95,7 +95,7 @@ class SRFHashTest extends TestCase {
 	protected function setUp(): void {
 		parent::setUp();
 		// Reset the static separator cache so tests are independent of execution order.
-		$ref = new \ReflectionProperty( SRFArray::class, 'mDefaultSeps' );
+		$ref = new \ReflectionProperty( ArrayFormat::class, 'mDefaultSeps' );
 		$ref->setAccessible( true );
 		$ref->setValue( null, [] );
 	}

commit 11c42ae81beac93e8d663fa56f190d802a9181e6

ArrayFormat: fix undeclared class error, make Arrays visible to Phan
createArray() calls the global \ExtArrays class unqualified while this
file is namespaced (SRF\ArrayFormat). PHP resolves an unqualified
static call relative to the current namespace, so without the leading
`\`, it looks for SRF\ArrayFormat\ExtArrays and throws an Error when
the Arrays extension is actually installed. Regression from fb1a1e0e.

Add Arrays as a CI dependency via extensions.local.json and widen
Phan's scope to include it (as already done for SemanticMediaWiki in
SemanticDrilldown/KnowledgeGraph/PageForms), so this class of bug is
caught going forward instead of silently skipped. Regenerate the
baseline accordingly.

.phan/baseline.php
@@ -485,7 +485,7 @@ return [
             'PhanUndeclaredClass' => ['\\SRF\\ArrayFormat\\ArrayPrinter::__construct', '\\SRF\\ArrayFormat\\ArrayPrinter::getParamDefinitions', '\\SRF\\ArrayFormat\\ArrayPrinter::handleParameters'],
             'PhanUndeclaredClassConstant' => ['\\SRF\\ArrayFormat\\ArrayPrinter::createArray', '\\SRF\\ArrayFormat\\ArrayPrinter::getCfgSepText', '\\SRF\\ArrayFormat\\ArrayPrinter::getQueryMode'],
             'PhanUndeclaredClassInstanceof' => ['\\SRF\\ArrayFormat\\ArrayPrinter::getResultText'],
-            'PhanUndeclaredClassMethod' => ['\\SRF\\ArrayFormat\\ArrayPrinter::createArray', '\\SRF\\ArrayFormat\\ArrayPrinter::deliverPropertiesManyValues', '\\SRF\\ArrayFormat\\ArrayPrinter::getResultText'],
+            'PhanUndeclaredClassMethod' => ['\\SRF\\ArrayFormat\\ArrayPrinter::deliverPropertiesManyValues', '\\SRF\\ArrayFormat\\ArrayPrinter::getResultText'],
             'PhanUndeclaredConstant' => ['\\SRF\\ArrayFormat\\ArrayPrinter::deliverPropertiesManyValues'],
             'PhanUndeclaredExtendedClass' => ['src/ArrayFormat/ArrayPrinter.php'],
             'PhanUndeclaredMethod' => ['\\SRF\\ArrayFormat\\ArrayPrinter::getCfgSepText'],

.phan/config.php
@@ -20,4 +20,24 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 	]
 );
 
+// Make optional dependency extensions visible to Phan's type-checker: those
+// activated in the Makefile / ci.yml matrix. mediawiki-phan-config only adds
+// MW core and MW vendor to directory_list by default, never extensions/.
+// Without this, calls into an optional extension's classes either surface as
+// PhanUndeclaredClass* noise, or (for unqualified class references resolved
+// relative to the current namespace) are silently skipped by Phan instead of
+// being checked against the dependency's actual API.
+$IP = getenv( 'MW_INSTALL_PATH' ) !== false
+	? str_replace( '\\', '/', getenv( 'MW_INSTALL_PATH' ) )
+	: '../..';
+
+$dependencyExtensions = [
+	'Arrays',
+];
+
+foreach ( $dependencyExtensions as $ext ) {
+	$cfg['directory_list'][] = $IP . '/extensions/' . $ext;
+	$cfg['exclude_analysis_directory_list'][] = $IP . '/extensions/' . $ext;
+}
+
 return $cfg;

src/ArrayFormat/ArrayPrinter.php
@@ -212,7 +212,7 @@ class ArrayPrinter extends ResultPrinter {
 
 		if ( defined( 'ExtArrays::VERSION' ) || class_exists( 'ExtArrays' ) ) {
 			$parser = MediaWikiServices::getInstance()->getParser();
-			ExtArrays::get( $parser )->createArray( $arrayId, $array );
+			\ExtArrays::get( $parser )->createArray( $arrayId, $array );
 			return true;
 		}
 

tests/phpunit/Unit/Formats/SRFArrayTest.php
@@ -553,4 +553,49 @@ class SRFArrayTest extends TestCase {
 		$this->assertFalse( $instance->createArrayPublic( [ 'a' ] ) );
 	}
 
+	/**
+	 * createArray() calls the global \ExtArrays class unqualified while this file
+	 * is itself namespaced (SRF\ArrayFormat). An unqualified class reference in a
+	 * static call is resolved by PHP relative to the current namespace, so without
+	 * the leading `\`, PHP looks for SRF\ArrayFormat\ExtArrays and throws an Error
+	 * when the Arrays extension is actually installed — regression for the
+	 * production crash reported when querying an inverse-array-name printout.
+	 */
+	public function testCreateArrayCallsGlobalExtArraysWhenArraysExtensionInstalled(): void {
+		if ( !class_exists( 'ExtArrays' ) ) {
+			// Minimal stand-in for the real Arrays extension's global ExtArrays
+			// class, matching its actual API (get(), instance createArray()).
+			eval( <<<'PHP'
+				class ExtArrays {
+					private static $instance;
+					public $created = [];
+					public static function &get( $parser ) {
+						if ( self::$instance === null ) {
+							self::$instance = new self();
+						}
+						return self::$instance;
+					}
+					public function createArray( $arrayId, array $array = [] ) {
+						$this->created[$arrayId] = $array;
+					}
+				}
+				PHP
+			);
+		}
+
+		$instance = new class( 'array' ) extends ArrayPrinter {
+			public function createArrayPublic( $array ) {
+				return parent::createArray( $array );
+			}
+			public function set( string $property, $value ): void {
+				$this->$property = $value;
+			}
+		};
+		$instance->set( 'mArrayName', 'myArr' );
+
+		$result = $instance->createArrayPublic( [ 'a', 'b' ] );
+
+		$this->assertTrue( $result );
+	}
+
 }